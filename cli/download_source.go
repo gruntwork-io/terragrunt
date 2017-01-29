354c2db829dbe6edf57394540323d40824f51dc6
Yevgeniy Brikman <brikis98@gmail.com>
2017-01-29 18:15:36 +0000

    Only download files if the source path has changed or is a local file path

--- cli/download_source.go
+++ cli/download_source.go
@@ -7,65 +7,111 @@ import (
 	"os"
 	"github.com/gruntwork-io/terragrunt/errors"
 	"path/filepath"
+	"github.com/hashicorp/go-getter"
+	urlhelper "github.com/hashicorp/go-getter/helper/url"
+	"encoding/base64"
 )
 
-// 1. Check out the given source URL, which should use Terraform's module source syntax, into a temporary folder
+type TerraformSource struct {
+	// The source path specified by the user
+	RawSource          string
+
+	// The canonical URL we compute from the source path
+	CanonicalSourceUrl string
+
+	// The folder where we should download the source to
+	DownloadFolder     string
+
+	// True if the source path points to a local file path and false otherwise
+	IsLocalSource      bool
+}
+
+// 1. Download the given source URL, which should use Terraform's module source syntax, into a temporary folder
 // 2. Copy the contents of terragruntOptions.WorkingDir into the temporary folder.
 // 3. Set terragruntOptions.WorkingDir to the temporary folder.
 func downloadTerraformSource(source string, terragruntOptions *options.TerragruntOptions) error {
-	tmpFolder, err := prepareTempDir(terragruntOptions)
+	terraformSource, err := processTerraformSource(source, terragruntOptions)
 	if err != nil {
 		return err
 	}
 
-	terragruntOptions.Logger.Printf("Downloading Terraform configurations from %s into %s", source, tmpFolder)
-	if err := terraformInit(source, tmpFolder, terragruntOptions); err != nil {
+	terragruntOptions.Logger.Printf("Using temporary folder %s for Terraform files from source %s", terraformSource.DownloadFolder, terraformSource.CanonicalSourceUrl)
+
+	// If the temp folder already exists, we assume we've already downloaded the Terraform files and don't need
+	// to spend time doing it again. The only exception is if the source URL points to a local file path, in which
+	// case the user is probably doing local, iterative development and we should assume the Terraform files have
+	// changed and need to be recopied every time (which is very fast anyway).
+	if !util.FileExists(terraformSource.DownloadFolder) {
+		if err := terraformInit(source, terraformSource.DownloadFolder, terragruntOptions); err != nil {
+			return err
+		}
+	} else if terraformSource.IsLocalSource {
+		if err := cleanupTerraformFiles(terraformSource.DownloadFolder, terragruntOptions); err != nil {
+			return err
+		}
+		if err := terraformInit(source, terraformSource.DownloadFolder, terragruntOptions); err != nil {
+			return err
+		}
+	} else {
+		terragruntOptions.Logger.Printf("Terraform files in %s are already up to date. Will not download again.", terraformSource.DownloadFolder)
+	}
+	
+	terragruntOptions.Logger.Printf("Copying files from %s into %s", terragruntOptions.WorkingDir, terraformSource.DownloadFolder)
+	if err := util.CopyFolderContents(terragruntOptions.WorkingDir, terraformSource.DownloadFolder); err != nil {
 		return err
 	}
 
-	terragruntOptions.Logger.Printf("Copying files from %s into %s", terragruntOptions.WorkingDir, tmpFolder)
-	if err := util.CopyFolderContents(terragruntOptions.WorkingDir, tmpFolder); err != nil {
-		return err
-	}
-
-	terragruntOptions.Logger.Printf("Setting working directory to %s", tmpFolder)
-	terragruntOptions.WorkingDir = tmpFolder
+	terragruntOptions.Logger.Printf("Setting working directory to %s", terraformSource.DownloadFolder)
+	terragruntOptions.WorkingDir = terraformSource.DownloadFolder
 
 	return nil
 }
 
-// Prepare a temp folder into which Terragrunt can download Terraform code. We take the canonical path of the current
-// working directory and create a folder under the system temporary folder with the same path. This allows us to reuse
-// the same temp folder for a given working directory so that if you run Terragrunt from the same folder multiple
-// times, we only have to download modules and configure remote state once (however, we download the source code every
-// time).
-func prepareTempDir(terragruntOptions *options.TerragruntOptions) (string, error) {
-	canonicalPath, err := util.CanonicalPath(terragruntOptions.WorkingDir, "")
+// Take the given source path and create a TerraformSource struct from it, including the folder where the source should
+// be downloaded to. We try to use the same download folder for a given source path by converting the source path to
+// a canonical form (e.g. converting relative file paths to absolute file paths) and using the base 64 encoded version
+// of the canonical path, within the OS tmp folder, as the download path. This way, if the source path doesn't change,
+// we don't have to unnecessarily re-download the source code, modules, and Terraform state.
+func processTerraformSource(source string, terragruntOptions *options.TerragruntOptions) (*TerraformSource, error) {
+	canonicalUrl, err := getter.Detect(source, terragruntOptions.WorkingDir, getter.Detectors)
 	if err != nil {
-		return "", err
+		return nil, errors.WithStackTrace(err)
 	}
 
-	tmpFolder := filepath.Join(os.TempDir(), "terragrunt-downloads", canonicalPath)
+	sourceUrl, err := urlhelper.Parse(canonicalUrl)
+	if err != nil {
+		return nil, errors.WithStackTrace(err)
+	}
 
-	if util.FileExists(tmpFolder) {
-		terragruntOptions.Logger.Printf("Temp folder %s already exists. Will delete Terraform configurations within it before downloading the latest ones.", tmpFolder)
-		if err := cleanupTerraformFiles(tmpFolder); err != nil {
-			return "", err
-		}
-	} else {
-		terragruntOptions.Logger.Printf("Creating temp folder %s to store downloaded Terraform configurations.", tmpFolder)
-		if err := os.MkdirAll(tmpFolder, 0777); err != nil {
-			return "", errors.WithStackTrace(err)
+	isLocalSource := sourceUrl.Scheme == "file"
+
+	if isLocalSource {
+		// Always use the canonical file path to ensure that a given path on the local file system is always
+		// represented the same way (i.e. doesn't differ just because the user provided a different relative
+		// path)
+		canonicalUrl, err = util.CanonicalPath(sourceUrl.Path, "")
+		if err != nil {
+			return nil, err
 		}
 	}
 
-	return tmpFolder, nil
+	canonicalUrlHash := base64.StdEncoding.EncodeToString([]byte(canonicalUrl))
+	downloadFolder := filepath.Join(os.TempDir(), "terragrunt-download", canonicalUrlHash)
+
+	return &TerraformSource{
+		RawSource: source,
+		CanonicalSourceUrl: canonicalUrl,
+		DownloadFolder: downloadFolder,
+		IsLocalSource: isLocalSource,
+	}, nil
 }
 
 // If this temp folder already exists, simply delete all the Terraform configurations (*.tf) within it
 // (the terraform init command will redownload the latest ones), but leave all the other files, such
 // as the .terraform folder with the downloaded modules and remote state settings.
-func cleanupTerraformFiles(path string) error {
+func cleanupTerraformFiles(path string, terragruntOptions *options.TerragruntOptions) error {
+	terragruntOptions.Logger.Printf("Cleaning up existing *.tf files in %s", path)
+
 	files, err := filepath.Glob(filepath.Join(path, "*.tf"))
 	if err != nil {
 		return errors.WithStackTrace(err)
@@ -88,6 +134,8 @@ func getTerraformSourceUrl(terragruntOptions *options.TerragruntOptions, terragr
 
 // Download the code from source into dest using the terraform init command
 func terraformInit(source string, dest string, terragruntOptions *options.TerragruntOptions) error {
+	terragruntOptions.Logger.Printf("Downloading Terraform configurations from %s into %s", source, dest)
+
 	terragruntInitOptions := terragruntOptions.Clone(terragruntOptions.TerragruntConfigPath)
 	terragruntInitOptions.TerraformCliArgs = []string{"init", source, dest}
 
