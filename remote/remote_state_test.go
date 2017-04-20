68a34a6753d1ba1434c5801407296b27170bf40b
Yevgeniy Brikman <brikis98@gmail.com>
2017-04-20 12:39:05 +0100

    Fix remote state test

--- remote/remote_state_test.go
+++ remote/remote_state_test.go
@@ -21,7 +21,7 @@ func TestToTerraformRemoteConfigArgs(t *testing.T) {
 	}
 	args := remoteState.toTerraformRemoteConfigArgs()
 
-	assertRemoteConfigArgsEqual(t, args, "remote config -backend s3 -backend-config=encrypt=true -backend-config=bucket=my-bucket -backend-config=key=terraform.tfstate -backend-config=region=us-east-1")
+	assertRemoteConfigArgsEqual(t, args, "init -backend-config=encrypt=true -backend-config=bucket=my-bucket -backend-config=key=terraform.tfstate -backend-config=region=us-east-1")
 }
 
 func TestToTerraformRemoteConfigArgsNoBackendConfigs(t *testing.T) {
@@ -30,7 +30,7 @@ func TestToTerraformRemoteConfigArgsNoBackendConfigs(t *testing.T) {
 	remoteState := RemoteState{Backend: "s3"}
 	args := remoteState.toTerraformRemoteConfigArgs()
 
-	assertRemoteConfigArgsEqual(t, args, "remote config -backend s3")
+	assertRemoteConfigArgsEqual(t, args, "init")
 }
 
 func TestShouldOverrideExistingRemoteState(t *testing.T) {
