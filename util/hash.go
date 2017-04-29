255c1096d7597cc600f1b2cd5bf88f731d933f31
Jocelyn Giroux <jocelyn_giroux@hotmail.com>
2017-04-29 16:24:34 -0400

    Fix problem on Windows with temp directory name encoding Fix problem with loosing errors on terraform init and terraform get

--- util/hash.go
+++ util/hash.go
@@ -3,10 +3,18 @@ package util
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"regexp"
 )
 
 // Returns the base 64 encoded sha1 hash of the given string
 func EncodeBase64Sha1(str string) string {
 	hash := sha1.Sum([]byte(str))
 	return base64.URLEncoding.EncodeToString(hash[:])
+}
+
+var removeNonAlphaNumericCharsRegex = regexp.MustCompile("[^a-zA-Z0-9]+")
+
+// Returns the base 64 encoded sha1 hash of the given string keeping only alphanumeric characters
+func EncodeBase64Sha1AlphaNum(str string) string {
+	return removeNonAlphaNumericCharsRegex.ReplaceAllString(EncodeBase64Sha1(str), "")
 }
