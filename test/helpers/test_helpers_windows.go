bcddad50d45777bd551aead4b41d37a03638aac4
Marco Reni <marco.reni@piksel.com>
2018-10-01 18:48:34 +0200

    test fix for windows: retrieve current root folder via os.Getwd

--- test/helpers/test_helpers_windows.go
+++ test/helpers/test_helpers_windows.go
@@ -2,4 +2,15 @@
 
 package helpers
 
-var RootFolder = "C:/"
+import (
+	"fmt"
+	"os"
+)
+
+var RootFolder = retrieveRootFolder()
+
+func retrieveRootFolder() string {
+	cwd, _ := os.Getwd();
+
+	return fmt.Sprintf("%s:/", cwd[0:1])
+}
