95310a5222630dd6127f104de5ed2cdb2aff7a47
Marc Tamsky <tamsky@users.noreply.github.com>
2019-05-06 15:25:09 -0700

    use defined const, skip testing --terragrunt-working-dir

--- test/integration_test.go
+++ test/integration_test.go
@@ -1475,14 +1475,14 @@ func TestTerragruntInfo(t *testing.T) {
 	showStdout := bytes.Buffer{}
 	showStderr := bytes.Buffer{}
 
-	err := runTerragruntCommand(t, fmt.Sprintf("terragrunt terragrunt-info --terragrunt-non-interactive --terragrunt-working-dir %s --terragrunt-download-dir %s", rootPath, rootPath+"/__TEST_DOWNLOAD_DIR__"), &showStdout, &showStderr)
+	err := runTerragruntCommand(t, fmt.Sprintf("terragrunt terragrunt-info --terragrunt-non-interactive --terragrunt-working-dir %s", rootPath), &showStdout, &showStderr)
 	logBufferContentsLineByLine(t, showStdout, "show stdout")
 
 	stdout := showStdout.String()
 
 	assert.Nil(t, err)
 	assert.Contains(t, stdout, "{")
-	assert.Contains(t, stdout, fmt.Sprintf(`"DownloadDir": "%s/__TEST_DOWNLOAD_DIR__"`, rootPath))
+	assert.Contains(t, stdout, fmt.Sprintf(`"DownloadDir": "%s/%s"`, rootPath, TERRAGRUNT_CACHE))
 	assert.Contains(t, stdout, `"TerraformCommand": "terragrunt-info",`)
 	assert.Contains(t, stdout, "}")
 }
