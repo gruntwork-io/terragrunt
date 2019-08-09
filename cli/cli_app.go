8810c8a1ee885e9eb923318623cb6b988557c676
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-08-08 22:21:10 -0700

    hclfmt should run before parsing the terragrunt config

--- cli/cli_app.go
+++ cli/cli_app.go
@@ -232,6 +232,10 @@ func runTerragrunt(terragruntOptions *options.TerragruntOptions) error {
 		return shell.RunTerraformCommand(terragruntOptions, terragruntOptions.TerraformCliArgs...)
 	}
 
+	if shouldRunHCLFmt(terragruntOptions) {
+		return runHCLFmt(terragruntOptions)
+	}
+
 	terragruntConfig, err := config.ReadTerragruntConfig(terragruntOptions)
 
 	if err != nil {
@@ -292,10 +296,6 @@ func runTerragrunt(terragruntOptions *options.TerragruntOptions) error {
 		}
 		fmt.Fprintf(terragruntOptions.Writer, "%s\n", b)
 		return nil
-	}
-
-	if shouldRunHCLFmt(terragruntOptions) {
-		return runHCLFmt(terragruntOptions)
 	}
 
 	if err := checkFolderContainsTerraformCode(terragruntOptions); err != nil {
