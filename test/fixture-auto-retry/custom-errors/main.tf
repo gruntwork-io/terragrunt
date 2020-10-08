e42953c9a6bcc34c71ef580ff395529ea3d0220d
Ana Krivokapic <ana@gruntwork.io>
2020-10-08 18:34:30 +0200

    Make the linter happy

--- test/fixture-auto-retry/custom-errors/main.tf
+++ test/fixture-auto-retry/custom-errors/main.tf
@@ -10,7 +10,7 @@ resource "null_resource" "tf_retryable_error" {
   provisioner "local-exec" {
     // The command will fail with a custom retryable error that matches the config the first time it's run,
     // and succeed on the 2nd run
-    command = "${path.module}/script.sh ${random_id.filename.hex}"
+    command     = "${path.module}/script.sh ${random_id.filename.hex}"
     interpreter = ["/bin/bash", "-c"]
   }
 }
