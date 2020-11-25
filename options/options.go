956796e2e75cd84b9e18f4fa8f3572a24c9cc54a
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-11-25 11:41:45 -0600

    Set OriginalTerraformCommand when creating new struct and add clarification about xxx-all commands

--- options/options.go
+++ options/options.go
@@ -51,6 +51,8 @@ type TerragruntOptions struct {
 	// TerraformCommand to `output` to run `terraform output`, which loses the context of the original command that was
 	// run to fetch the dependency. This is a problem when mock_outputs is configured and we only allow mocks to be
 	// returned on specific commands.
+	// NOTE: For `xxx-all` commands, this will be set to the Terraform command, which would be `xxx`. For example,
+	// if you run `apply-all` (which is a terragrunt command), this variable will be set to `apply`.
 	OriginalTerraformCommand string
 
 	// Version of terraform (obtained by running 'terraform version')
@@ -167,6 +169,7 @@ func NewTerragruntOptions(terragruntConfigPath string) (*TerragruntOptions, erro
 	return &TerragruntOptions{
 		TerragruntConfigPath:        terragruntConfigPath,
 		TerraformPath:               TERRAFORM_DEFAULT_PATH,
+		OriginalTerraformCommand:    "",
 		TerraformCommand:            "",
 		AutoInit:                    true,
 		NonInteractive:              false,
