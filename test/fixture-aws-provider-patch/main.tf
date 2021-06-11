98f59370b89f7c60ad89284cde26271cbf04f986
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2021-06-11 10:29:33 -0500

    Fix data type issue with aws-provider-patch (#1714)
    * Regression test for issue #1709
    
    * Use json as an intermediary representation for aws-provider-patch
    
    * Fix unit tests

--- test/fixture-aws-provider-patch/main.tf
+++ test/fixture-aws-provider-patch/main.tf
@@ -1,10 +1,12 @@
 provider "aws" {
-  region = var.primary_aws_region
+  region              = var.primary_aws_region
+  allowed_account_ids = var.allowed_account_ids
 }
 
 module "example_module" {
   source = "github.com/gruntwork-io/terragrunt.git//test/fixture-aws-provider-patch/example-module?ref=__BRANCH_NAME__"
 
+  allowed_account_ids  = var.allowed_account_ids
   secondary_aws_region = var.secondary_aws_region
   bucket_name          = var.bucket_name
 }
@@ -17,6 +19,11 @@ variable "primary_aws_region" {
 variable "secondary_aws_region" {
   description = "The secondary AWS region to deploy the S3 bucket from the module into"
   type        = string
+}
+
+variable "allowed_account_ids" {
+  description = "The list of IDs of AWS accounts that are allowed to run this module."
+  type        = list(string)
 }
 
 variable "bucket_name" {
