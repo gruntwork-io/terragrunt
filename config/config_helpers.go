b8013b10e318603c7dfdcd6a0945c7f61f5df28e
Roman <pertsev.roman@gmail.com>
2021-07-09 00:46:30 +0300

    additional provider params

--- config/config_helpers.go
+++ config/config_helpers.go
@@ -102,6 +102,7 @@ type ProviderHandler struct {
 var providerImpls = map[string]ProviderHandler{
 	"aws": {
 		Param: cty.ObjectWithOptionalAttrs(map[string]cty.Type{
+			"alias":                   cty.String,
 			"access_key":              cty.String,
 			"secret_key":              cty.String,
 			"region":                  cty.String,
@@ -111,7 +112,19 @@ var providerImpls = map[string]ProviderHandler{
 			"max_retries":             cty.String,
 			"allowed_account_ids":     cty.List(cty.String),
 			"forbidden_account_ids":   cty.List(cty.String),
+			//"default_tags":            cty.List(cty.String),
+			"ignore_tags": cty.List(cty.String),
+			"insecure":    cty.Bool,
+
+			// boolean flags block
+			"skip_credentials_validation": cty.Bool,
+			"skip_get_ec2_platforms":      cty.Bool,
+			"skip_region_validation":      cty.Bool,
+			"skip_requesting_account_id":  cty.Bool,
+			"skip_metadata_api_check":     cty.Bool,
+			"s3_force_path_style":         cty.Bool,
 		}, []string{
+			"alias",
 			"access_key",
 			"secret_key",
 			"region",
@@ -121,6 +134,17 @@ var providerImpls = map[string]ProviderHandler{
 			"max_retries",
 			"allowed_account_ids",
 			"forbidden_account_ids",
+			"default_tags",
+			"ignore_tags",
+			"insecure",
+
+			// boolean flags block
+			"skip_credentials_validation",
+			"skip_get_ec2_platforms",
+			"skip_region_validation",
+			"skip_requesting_account_id",
+			"skip_metadata_api_check",
+			"s3_force_path_style",
 		}),
 		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
 			return cty.StringVal(""), nil
