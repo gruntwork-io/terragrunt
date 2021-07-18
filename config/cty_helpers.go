cb655a4fbd085849d69ea8eb170818b51448a520
Roman <pertsev.roman@gmail.com>
2021-07-18 16:44:19 +0300

    templatefile implementation

--- config/cty_helpers.go
+++ config/cty_helpers.go
@@ -2,13 +2,16 @@ package config
 
 import (
 	"encoding/json"
-
 	"github.com/gruntwork-io/terragrunt/errors"
 	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
 	"github.com/zclconf/go-cty/cty/gocty"
 	ctyjson "github.com/zclconf/go-cty/cty/json"
+	"io/ioutil"
+	"path/filepath"
 )
 
 // Create a cty Function that takes as input parameters a slice of strings (var args, so this slice could be of any
@@ -56,6 +59,105 @@ func wrapVoidToStringAsFuncImpl(toWrap func(include *IncludeConfig, terragruntOp
 				return cty.StringVal(""), err
 			}
 			return cty.StringVal(out), nil
+		},
+	})
+}
+
+func templateFileFunctionImpl(basePath string) function.Function {
+	params := []function.Parameter{
+		{
+			Name: "path",
+			Type: cty.String,
+		},
+		{
+			Name: "vars",
+			Type: cty.DynamicPseudoType,
+		},
+	}
+
+	loadTmpl := func(filename string) (hcl.Expression, error) {
+		absFileName := filepath.Join(basePath, filename)
+
+		file, err := ioutil.ReadFile(absFileName)
+
+		if err != nil {
+			return nil, err
+		}
+
+		expr, diags := hclsyntax.ParseTemplate(file, filename, hcl.Pos{Line: 1, Column: 1})
+		if diags.HasErrors() {
+			return nil, diags
+		}
+
+		return expr, nil
+	}
+
+	renderTmpl := func(expr hcl.Expression, varsVal cty.Value) (cty.Value, error) {
+		if varsTy := varsVal.Type(); !(varsTy.IsMapType() || varsTy.IsObjectType()) {
+			return cty.DynamicVal, function.NewArgErrorf(1, "invalid vars value: must be a map") // or an object, but we don't strongly distinguish these most of the time
+		}
+
+		ctx := &hcl.EvalContext{
+			Variables: varsVal.AsValueMap(),
+		}
+
+		// We require all of the variables to be valid HCL identifiers, because
+		// otherwise there would be no way to refer to them in the template
+		// anyway. Rejecting this here gives better feedback to the user
+		// than a syntax error somewhere in the template itself.
+		for n := range ctx.Variables {
+			if !hclsyntax.ValidIdentifier(n) {
+				// This error message intentionally doesn't describe _all_ of
+				// the different permutations that are technically valid as an
+				// HCL identifier, but rather focuses on what we might
+				// consider to be an "idiomatic" variable name.
+				return cty.DynamicVal, function.NewArgErrorf(1, "invalid template variable name %q: must start with a letter, followed by zero or more letters, digits, and underscores", n)
+			}
+		}
+
+		// We'll pre-check references in the template here so we can give a
+		// more specialized error message than HCL would by default, so it's
+		// clearer that this problem is coming from a templatefile call.
+		for _, traversal := range expr.Variables() {
+			root := traversal.RootName()
+			if _, ok := ctx.Variables[root]; !ok {
+				return cty.DynamicVal, function.NewArgErrorf(1, "vars map does not contain key %q, referenced at %s", root, traversal[0].SourceRange())
+			}
+		}
+
+		val, diags := expr.Value(ctx)
+		if diags.HasErrors() {
+			return cty.DynamicVal, diags
+		}
+		return val, nil
+	}
+
+	return function.New(&function.Spec{
+		Params: params,
+		Type: func(args []cty.Value) (cty.Type, error) {
+			if !(args[0].IsKnown() && args[1].IsKnown()) {
+				return cty.DynamicPseudoType, nil
+			}
+
+			// We'll render our template now to see what result type it produces.
+			// A template consisting only of a single interpolation an potentially
+			// return any type.
+			expr, err := loadTmpl(args[0].AsString())
+			if err != nil {
+				return cty.DynamicPseudoType, err
+			}
+
+			// This is safe even if args[1] contains unknowns because the HCL
+			// template renderer itself knows how to short-circuit those.
+			val, err := renderTmpl(expr, args[1])
+			return val.Type(), err
+		},
+		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+			expr, err := loadTmpl(args[0].AsString())
+			if err != nil {
+				return cty.DynamicVal, err
+			}
+			return renderTmpl(expr, args[1])
 		},
 	})
 }
--- test/fixture-providers/terragrunt.hcl
+++ test/fixture-providers/terragrunt.hcl
@@ -1,18 +1,14 @@
 locals {
-  aws_region = "us-east-1"
+  aws_region      = "us-east-1"
+  assume_role_arn = "arn:aws:iam::1234567890:role/assume-role"
 }
 
 generate "test-provider" {
   path      = "test-provider.tf"
   if_exists = "overwrite_terragrunt"
 
-  contents = make_aws_provider({
-    region                      = local.aws_region
-    allowed_account_ids         = ["1234567890"]
-    skip_credentials_validation = true
-    assume_role = {
-      external_id      = "1"
-      duration_seconds = 3600
-    }
+  contents = templatefile("${path_relative_to_include()}/providers.tmpl", {
+    aws_region      = local.aws_region,
+    assume_role_arn = local.assume_role_arn
   })
 }
