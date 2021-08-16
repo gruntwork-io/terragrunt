95d861031cfae37dc6442a5fbf11f44e992f51cf
yorinasub17 <430092+yorinasub17@users.noreply.github.com>
2021-08-16 15:27:15 -0500

    Add context about why tfr package is internal

--- internal/tfr/doc.go
+++ internal/tfr/doc.go
@@ -2,6 +2,9 @@
 // Protocol documented in the official terraform docs:
 // https://www.terraform.io/docs/internals/module-registry-protocol.html
 //
-// MAINTAINER'S NOTE: This package is intentionally set to internal as we do not want to support usage of this outside
-// of Terragrunt.
+// MAINTAINER'S NOTE: Ideally we would be able to reuse code from Terraform. However, terraform has moved to packaging
+// all its libraries under internal so that you can't use them as a library outside of Terraform. To respect the
+// direction and spirit of the Terraform team, we opted for not doing anything funky to workaround the limitation (like
+// copying those files in here). We also opted to keep this functionality internal to align with the Terraform team's
+// decision to not support client libraries for accessing the Terraform Registry.
 package tfr
