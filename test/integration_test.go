2dfbe8618617ed06baaf4c4a1a68225027e400b1
Zack Proser <zackproser@gmail.com>
2021-08-25 09:42:29 -0400

    Update test/integration_test.go

--- test/integration_test.go
+++ test/integration_test.go
@@ -3682,7 +3682,6 @@ func copyEnvironmentToPath(t *testing.T, environmentPath, targetPath string) {
 		t.Fatalf("Failed to create temp dir %s due to error %v", targetPath, err)
 	}
 
-
 	copyErr := util.CopyFolderContents(environmentPath, util.JoinPath(targetPath, environmentPath), ".terragrunt-test")
 	fmt.Println(copyErr)
 }
