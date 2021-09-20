2f1efd0626755b2c5253d4de8c739666f50776c3
Denis Obada <denis.o@linux.com>
2021-09-20 11:04:49 +0300

    Updated reference to TestWindowsTerragruntSourceMapDebug for windows tests

--- test/integration_windows_test.go
+++ test/integration_windows_test.go
@@ -25,7 +25,7 @@ func TestWindowsLocalWithRelativeExtraArgsWindows(t *testing.T) {
 	runTerragrunt(t, fmt.Sprintf("terragrunt apply -auto-approve --terragrunt-non-interactive --terragrunt-working-dir %s", TEST_FIXTURE_LOCAL_RELATIVE_ARGS_WINDOWS_DOWNLOAD_PATH))
 }
 
-// TestTerragruntSourceMapDebug copies the test/fixture-source-map directory to a new Windows path
+// TestWindowsTerragruntSourceMapDebug copies the test/fixture-source-map directory to a new Windows path
 // and then ensures that the TERRAGRUNT_SOURCE_MAP env var can be used to swap out git sources for local modules
 func TestWindowsTerragruntSourceMapDebug(t *testing.T) {
 	fixtureSourceMapPath := "fixture-source-map"
