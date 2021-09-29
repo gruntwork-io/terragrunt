a5aab86dafc6562fc52dd8dcee957863576eabc7
yorinasub17 <430092+yorinasub17@users.noreply.github.com>
2021-09-29 13:07:27 -0500

    Revert #1811 to restore useful error message when dependency output is missing

--- test/fixture-include-no-output/app/main.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "main_file" {
-  content  = "main_file"
-  filename = "${path.module}/test_file.tfstate"
-}
-
-output "main_file" {
-  value = local_file.main_file.filename
-}
-
--- test/integration_test.go
+++ test/integration_test.go
@@ -127,7 +127,6 @@ const (
 	TEST_FIXTURE_DIRS_PATH                                  = "fixture-dirs"
 	TEST_FIXTURE_PARALLELISM                                = "fixture-parallelism"
 	TEST_FIXTURE_SOPS                                       = "fixture-sops"
-	TEST_FIXTURE_INCLUDE_NO_OUTPUT                          = "fixture-include-no-output"
 	TEST_FIXTURE_DESTROY_WARNING                            = "fixture-destroy-warning"
 	TERRAFORM_BINARY                                        = "terraform"
 	TERRAFORM_FOLDER                                        = ".terraform"
@@ -2266,7 +2265,7 @@ func TestDependencyOutputErrorBeforeApply(t *testing.T) {
 	err := runTerragruntCommand(t, fmt.Sprintf("terragrunt plan --terragrunt-non-interactive --terragrunt-working-dir %s", app3Path), &showStdout, &showStderr)
 	assert.Error(t, err)
 	// Verify that we fail because the dependency is not applied yet
-	assert.Contains(t, err.Error(), "object does not have an attribute named \"outputs\"")
+	assert.Contains(t, err.Error(), "has not been applied yet")
 
 	logBufferContentsLineByLine(t, showStdout, "show stdout")
 	logBufferContentsLineByLine(t, showStderr, "show stderr")
@@ -2595,7 +2594,7 @@ func TestDependencyMockOutputRestricted(t *testing.T) {
 	err := runTerragruntCommand(t, fmt.Sprintf("terragrunt apply -auto-approve --terragrunt-non-interactive --terragrunt-working-dir %s", dependent2Path), &showStdout, &showStderr)
 	assert.Error(t, err)
 	// Verify that we fail because the dependency is not applied yet
-	assert.Contains(t, err.Error(), "object does not have an attribute named \"outputs\"")
+	assert.Contains(t, err.Error(), "has not been applied yet")
 
 	logBufferContentsLineByLine(t, showStdout, "show stdout")
 	logBufferContentsLineByLine(t, showStderr, "show stderr")
@@ -4307,24 +4306,6 @@ func TestTerragruntInitRunCmd(t *testing.T) {
 	assert.Equal(t, 3, strings.Count(errout, "uuid"))
 	assert.Equal(t, 4, strings.Count(errout, "random_arg"))
 	assert.Equal(t, 3, strings.Count(errout, "another_arg"))
-}
-
-func TestNoFailureForModulesWithoutOutputs(t *testing.T) {
-
-	appPath := util.JoinPath(TEST_FIXTURE_INCLUDE_NO_OUTPUT, "app")
-
-	cleanupTerraformFolder(t, appPath)
-	stdout := bytes.Buffer{}
-	stderr := bytes.Buffer{}
-
-	err := runTerragruntCommand(t, fmt.Sprintf("terragrunt run-all init --terragrunt-non-interactive --terragrunt-working-dir %s", appPath), &stdout, &stderr)
-	assert.NoError(t, err)
-
-	stdout = bytes.Buffer{}
-	stderr = bytes.Buffer{}
-
-	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt run-all apply --terragrunt-non-interactive --terragrunt-working-dir %s", appPath), &stdout, &stderr)
-	assert.NoError(t, err)
 }
 
 func TestShowWarningWithDependentModulesBeforeDestroy(t *testing.T) {
