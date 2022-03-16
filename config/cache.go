76ef619f90d74071e866aa0430c90f901801b43b
Denis O <denis.o@linux.com>
2022-03-16 16:27:45 +0200

    #1971 Added caching of IAM roles parsed from HCL files (#2010)
    * Added caching of IAM role
    
    * Updated description
    
    * Replaced md5 checksum calculation with SHA-256

--- config/cache.go
+++ config/cache.go
@@ -1,9 +1,11 @@
 package config
 
 import (
-	"crypto/md5"
+	"crypto/sha256"
 	"fmt"
 	"sync"
+
+	"github.com/gruntwork-io/terragrunt/options"
 )
 
 // StringCache - structure to store cached values
@@ -24,8 +26,8 @@ func NewStringCache() *StringCache {
 func (cache *StringCache) Get(key string) (string, bool) {
 	cache.Mutex.Lock()
 	defer cache.Mutex.Unlock()
-	md5Sum := md5.Sum([]byte(key))
-	cacheKey := fmt.Sprintf("%x", md5Sum)
+	keyHash := sha256.Sum256([]byte(key))
+	cacheKey := fmt.Sprintf("%x", keyHash)
 	value, found := cache.Cache[cacheKey]
 	return value, found
 }
@@ -34,7 +36,40 @@ func (cache *StringCache) Get(key string) (string, bool) {
 func (cache *StringCache) Put(key string, value string) {
 	cache.Mutex.Lock()
 	defer cache.Mutex.Unlock()
-	md5Sum := md5.Sum([]byte(key))
-	cacheKey := fmt.Sprintf("%x", md5Sum)
+	keyHash := sha256.Sum256([]byte(key))
+	cacheKey := fmt.Sprintf("%x", keyHash)
+	cache.Cache[cacheKey] = value
+}
+
+// IAMRoleOptionsCache - cache for IAMRole options
+type IAMRoleOptionsCache struct {
+	Cache map[string]options.IAMRoleOptions
+	Mutex *sync.Mutex
+}
+
+// NewIAMRoleOptionsCache - create new cache for IAM roles
+func NewIAMRoleOptionsCache() *IAMRoleOptionsCache {
+	return &IAMRoleOptionsCache{
+		Cache: map[string]options.IAMRoleOptions{},
+		Mutex: &sync.Mutex{},
+	}
+}
+
+// Get - get cached value, md5 hash is used as key to have fixed length keys and avoid duplicates
+func (cache *IAMRoleOptionsCache) Get(key string) (options.IAMRoleOptions, bool) {
+	cache.Mutex.Lock()
+	defer cache.Mutex.Unlock()
+	keyHash := sha256.Sum256([]byte(key))
+	cacheKey := fmt.Sprintf("%x", keyHash)
+	value, found := cache.Cache[cacheKey]
+	return value, found
+}
+
+// Put - put value in cache, md5 hash is used as key to have fixed length keys and avoid duplicates
+func (cache *IAMRoleOptionsCache) Put(key string, value options.IAMRoleOptions) {
+	cache.Mutex.Lock()
+	defer cache.Mutex.Unlock()
+	keyHash := sha256.Sum256([]byte(key))
+	cacheKey := fmt.Sprintf("%x", keyHash)
 	cache.Cache[cacheKey] = value
 }
