987deca7ea905608ac194d64878679be03ad70e9
Denis O <denis.o@linux.com>
2022-05-17 15:10:01 +0300

    Updated reading of AWS reponse using BoolValue function

--- remote/remote_state_s3.go
+++ remote/remote_state_s3.go
@@ -1152,16 +1152,16 @@ func checkIfS3PublicAccessBlockingEnabled(s3Client *s3.S3, config *RemoteStateCo
 		return false, nil
 	}
 
-	if !*output.PublicAccessBlockConfiguration.BlockPublicAcls {
+	if !aws.BoolValue(output.PublicAccessBlockConfiguration.BlockPublicAcls) {
 		return false, nil
 	}
-	if !*output.PublicAccessBlockConfiguration.BlockPublicPolicy {
+	if !aws.BoolValue(output.PublicAccessBlockConfiguration.BlockPublicAcls) {
 		return false, nil
 	}
-	if !*output.PublicAccessBlockConfiguration.IgnorePublicAcls {
+	if !aws.BoolValue(output.PublicAccessBlockConfiguration.BlockPublicAcls) {
 		return false, nil
 	}
-	if !*output.PublicAccessBlockConfiguration.RestrictPublicBuckets {
+	if !aws.BoolValue(output.PublicAccessBlockConfiguration.BlockPublicAcls) {
 		return false, nil
 	}
 
