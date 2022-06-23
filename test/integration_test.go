870b54147dcdf4f674656c7623ef481cac633817
Denis O <denis.o@linux.com>
2022-06-23 18:26:33 +0000

    Init tests cleanup

--- test/integration_test.go
+++ test/integration_test.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"net/url"
 	"os"
@@ -138,10 +137,6 @@ const (
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-	output, err := exec.Command("git", "config", "--global", "--add", "safe.directory", os.TempDir()).CombinedOutput()
-	if err != nil {
-		log.Fatal(fmt.Errorf("Failed set temporary git safe directory %v\n%s", err, string(output)))
-	}
 }
 
 func TestTerragruntInitHookNoSourceNoBackend(t *testing.T) {
