665d3caaae5a119795f52c2588ed7632baafdb6b
Denis O <denis.o@linux.com>
2022-10-04 18:22:34 +0300

    PR cleanup

--- cli/render_json.go
+++ cli/render_json.go
@@ -27,6 +27,7 @@ const renderJsonHelp = `
    --with-metadata 		Add metadata to the rendered JSON file.
    --terragrunt-json-out 	The file path that terragrunt should use when rendering the terragrunt.hcl config as json.
 `
+
 // runRenderJSON takes the parsed TerragruntConfig struct and renders it out as JSON so that it can be processed by
 // other tools. To make it easier to maintain, this uses the cty representation as an intermediary.
 // NOTE: An unspecified advantage of using the cty representation is that the final block outputs would be a map
