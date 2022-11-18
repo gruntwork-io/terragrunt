c0f1a784b261ad7b97d140339e655f4c9b355186
Denis O <denis.o@linux.com>
2022-11-18 09:25:33 +0000

    Choco usage test

--- _ci/install-golang.ps1
+++ _ci/install-golang.ps1
@@ -1,19 +1,5 @@
-# remove the old go installation
-$goPath = "C:\Program Files\Go"
-$goTmp = "C:\Gotmp\go"
-$golangURI = "https://golang.org/dl/go1.18.8.windows-amd64.zip"
-$output = "go1.18.8.zip"
-if (Test-Path -Path $goPath)
-{
-    Remove-Item $goPath -Recurse
-}
-# Download golang, unpack it, and then update the PATH to include gobin
-# The SilentlyContinue is needed to handle access denied error. See
-# https://discuss.circleci.com/t/access-denied-error-while-trying-to-download-software-on-windows-cirlcleci-environment/32809/2
-$ProgressPreference = "SilentlyContinue"
-Invoke-WebRequest -Uri $golangURI -OutFile $output
-Expand-Archive -LiteralPath $output -DestinationPath $goTmp
-Move-Item $goTmp $goPath
+# Usage of Chocolatey to install golang
+choco install golang --version 1.18.5 -y
 # Verify installation
 Get-Command go
 go version
