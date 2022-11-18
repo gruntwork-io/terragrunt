c7867c7646e1153bc6f3b841953ad4dcca5482a9
Denis O <denis.o@linux.com>
2022-11-18 08:59:56 +0000

    Debug go installation in windows

--- _ci/install-golang.ps1
+++ _ci/install-golang.ps1
@@ -1,10 +1,12 @@
 # remove the old go installation
-if (test-path "C:\Go") {
-    Remove-Item "C:\Go" -Recurse
-}
-# Download golang, unpack it, and then update the PATH to include gobin
+$goPath = "C:\Go"
 $golangURI = "https://golang.org/dl/go1.18.8.windows-amd64.zip"
 $output = "go1.18.8.zip"
+if (Test-Path -Path $goPath)
+{
+    Remove-Item $goPath -Recurse
+}
+# Download golang, unpack it, and then update the PATH to include gobin
 # The SilentlyContinue is needed to handle access denied error. See
 # https://discuss.circleci.com/t/access-denied-error-while-trying-to-download-software-on-windows-cirlcleci-environment/32809/2
 $ProgressPreference = "SilentlyContinue"
@@ -13,3 +15,4 @@ Expand-Archive -LiteralPath $output -DestinationPath "C:\Gotmp"
 Move-Item "C:\Gotmp\go" "C:\Go"
 # Verify installation
 go version
+Get-Command go
