a83a7b8a0f4cb147f9637ecf9200be83c8ea3883
Denis O <denis.o@linux.com>
2022-11-24 17:43:36 +0000

    Go 1.18 upgrade (#2365)
    * go 1.18 upgrade
    
    * Add powershell test for go directory
    
    * Debug go installation in windows
    
    * Updated windows installation path
    
    * Add usage of temporary go directory
    
    * Choco usage test
    
    * Simplified package installation
    
    * Windows installation cleanup

--- _ci/install-terraform.ps1
+++ _ci/install-terraform.ps1
@@ -1,29 +1,5 @@
-TerraformInstallPath = "C:\Program Files\Terraform\terraform.exe"
-$TerraformTmpPath = "C:\Terraformtmp"
-$TerraformTmpBinaryPath = "C:\Terraformtmp\terraform.exe"
-$TerraformPath = "C:\Program Files\Terraform"
-# Remove any old terraform installation, if present
-if (Test-Path -Path $TerraformInstallPath)
-{
-	Remove-Item $TerraformInstallPath -Recurse
-}
-# Download terraform and unpack it
-$terraformURI = "https://releases.hashicorp.com/terraform/1.3.2/terraform_1.3.2_windows_amd64.zip"
-$output = "terraform.1.3.2.zip"
-$ProgressPreference = "SilentlyContinue"
-Invoke-WebRequest -Uri $terraformURI -OutFile $output
-New-Item -ItemType "directory" -Path $TerraformTmpPath
-# Unpack Terraform to temp directory
-Expand-Archive -LiteralPath $output -DestinationPath $TerraformTmpPath
-# Make new Terraform directory to hold binary
-New-Item -ItemType "directory" -Path $TerraformPath
-Move-Item $TerraformTmpBinaryPath $TerraformPath
-# Add new Terraform path to system
-$OldPath = [System.Environment]::GetEnvironmentVariable('PATH', "Machine")
-$NewPath = "$OldPath;$TerraformPath"
-[Environment]::SetEnvironmentVariable("PATH", "$NewPath", "Machine")
-# Load System and User PATHs into latest $env:Path, which has the effect of "refreshing" the latest path
-# in the current PowerShell session
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+# Install terraform using Chocolatey
+choco install terraform --version 1.3.2 -y
 # Verify installation
+Get-Command terraform
 terraform version
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/gruntwork-io/terragrunt
 
-go 1.17
+go 1.18
 
 require (
 	cloud.google.com/go/storage v1.16.0
