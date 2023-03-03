80b79f962a16e21b7b99905d69580cfaae93c0b7
Denis O <denis.o@linux.com>
2023-03-03 13:32:30 +0000

    Test cleanup

--- test/integration_tflint_test.go
+++ test/integration_tflint_test.go
@@ -105,7 +105,7 @@ func TestTflintFindsNoIssuesWithValidCodeDifferentDownloadDir(t *testing.T) {
 
 	assert.NotContains(t, errOut.String(), "Error while running tflint with args:")
 	assert.NotContains(t, errOut.String(), "Tflint found issues in the project. Check for the tflint logs above.")
-	found, err := regexp.MatchString(fmt.Sprintf("--config %s/.terragrunt-cache/.*/.tflint.hcl", downloadDir), errOut.String())
+	found, err := regexp.MatchString(fmt.Sprintf("--config %s/.*/.tflint.hcl", downloadDir), errOut.String())
 	assert.NoError(t, err)
 	assert.True(t, found)
 }
