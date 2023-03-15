27d988c3d5b5e63e65d647eb6da0815ed0a6b7a3
Denis O <denis.o@linux.com>
2023-03-15 19:51:11 +0000

    Update TF version

--- _ci/install-terraform.ps1
+++ _ci/install-terraform.ps1
@@ -1,5 +1,5 @@
 # Install terraform using Chocolatey
-choco install terraform --version 1.4.1 -y
+choco install terraform --version 1.3.2 -y
 # Verify installation
 Get-Command terraform
 terraform version
