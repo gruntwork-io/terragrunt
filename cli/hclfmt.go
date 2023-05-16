b462b23efaa1cc00c717f7682fb7b477b3bf22e4
Mikhail Naletov <okgolove@markeloff.net>
2023-05-16 19:17:23 +0300

    feat: implement hclfmt diff output

--- cli/hclfmt.go
+++ cli/hclfmt.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -93,6 +94,14 @@ func formatTgHCL(terragruntOptions *options.TerragruntOptions, tgHclFile string)
 
 	fileUpdated := !bytes.Equal(newContents, contents)
 
+	if terragruntOptions.Diff && fileUpdated {
+		diff, err := bytesDiff(contents, newContents, tgHclFile)
+		if err != nil {
+			return fmt.Errorf("Failed to generate diff for %s", tgHclFile)
+		}
+		fmt.Printf("%s\n", diff)
+	}
+
 	if terragruntOptions.Check && fileUpdated {
 		return fmt.Errorf("Invalid file format %s", tgHclFile)
 	}
@@ -115,4 +124,28 @@ func checkErrors(logger *logrus.Entry, contents []byte, tgHclFile string) error 
 		return diags
 	}
 	return nil
+}
+
+func bytesDiff(b1, b2 []byte, path string) (data []byte, err error) {
+	f1, err := ioutil.TempFile("", "")
+	if err != nil {
+		return
+	}
+	defer os.Remove(f1.Name())
+	defer f1.Close()
+	f2, err := ioutil.TempFile("", "")
+	if err != nil {
+		return
+	}
+	defer os.Remove(f2.Name())
+	defer f2.Close()
+	f1.Write(b1)
+	f2.Write(b2)
+	data, err = exec.Command("diff", "--label=old/"+path, "--label=new/"+path, "-u", f1.Name(), f2.Name()).CombinedOutput()
+	if len(data) > 0 {
+		// diff exits with a non-zero status when the files don't match.
+		// Ignore that failure as long as we get output.
+		err = nil
+	}
+	return
 }
