8202aedb39286ed7ae0c96a56c5bcbb1ff323c28
Denis O <denis.o@linux.com>
2023-06-07 14:09:31 +0000

    Ref reconstruction

--- config/config.go
+++ config/config.go
@@ -10,6 +10,8 @@ import (
 	"reflect"
 	"strings"
 
+	urlhelper "github.com/hashicorp/go-getter/helper/url"
+
 	"github.com/hashicorp/go-getter"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
@@ -530,6 +532,20 @@ func adjustSourceWithMap(sourceMap map[string]string, source string, modulePath 
 			return moduleSubdirFromUrl, err
 		}
 		moduleSubdir = moduleSubdirFromUrl
+	}
+	// if source path contains "?ref=", reconstruct module dir using "//"
+	if strings.Contains(sourcePath, "?ref=") && moduleSubdir != "" {
+		canonicalSourceUrl, err := urlhelper.Parse(sourcePath)
+		if err != nil {
+			return "", errors.WithStackTrace(err)
+		}
+		// append moduleSubdir to the path
+		if canonicalSourceUrl.Opaque != "" {
+			canonicalSourceUrl.Opaque += fmt.Sprintf("//%s", moduleSubdir)
+		} else {
+			canonicalSourceUrl.Path += fmt.Sprintf("//%s", moduleSubdir)
+		}
+		return canonicalSourceUrl.String(), nil
 	}
 	return util.JoinTerraformModulePath(sourcePath, moduleSubdir), nil
 
