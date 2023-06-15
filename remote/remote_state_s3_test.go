b23931b37686fed0d0bbb0dc72df450700f9f302
Denis O <denis.o@linux.com>
2023-06-15 08:31:13 +0000

    Go 1.20 formatting update

--- remote/remote_state_s3_test.go
+++ remote/remote_state_s3_test.go
@@ -351,7 +351,7 @@ func TestGetTerraformInitArgs(t *testing.T) {
 }
 
 // Test to validate cases when is not possible to read all S3 configurations
-//https://github.com/gruntwork-io/terragrunt/issues/2109
+// https://github.com/gruntwork-io/terragrunt/issues/2109
 func TestNegativePublicAccessResponse(t *testing.T) {
 	t.Parallel()
 	testCases := []struct {
