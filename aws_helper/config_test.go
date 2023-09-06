d3285f0e44a7b82a1a20598088f529004459c2c7
Denis O <denis.o@linux.com>
2023-09-06 11:56:07 -0700

    Terragrunt version in AWS API Calls (#2701)
    * Add AWS session handler to add user agent in AWS requests
    
    * Add test for user agent checking

---
+++ aws_helper/config_test.go
@@ -0,0 +1,30 @@
+package aws_helper
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerragruntIsAddedInUserAgent(t *testing.T) {
+	t.Parallel()
+
+	sess, err := CreateAwsSession(nil, options.NewTerragruntOptions())
+	assert.NoError(t, err)
+
+	op := &request.Operation{
+		Name:       "",
+		HTTPMethod: "POST",
+		HTTPPath:   "/",
+	}
+	input := &sts.GetCallerIdentityInput{}
+	output := &sts.GetCallerIdentityOutput{}
+
+	r := sts.New(sess).NewRequest(op, input, output)
+	sess.Handlers.Build.Run(r)
+
+	assert.Contains(t, r.HTTPRequest.Header.Get("User-Agent"), "terragrunt")
+}
