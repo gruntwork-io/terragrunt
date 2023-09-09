77d8efc5dd2e527926a66a26901fff6824a48dc2
Levko Burburas <levko.himins@biptec.com>
2023-09-09 07:20:02 +0300

    fix: converting to absolute path

--- cli/app.go
+++ cli/app.go
@@ -155,7 +155,7 @@ func initialSetup(opts *options.TerragruntOptions) func(ctx *cli.Context) error 
 		// --- Terragrunt ConfigPath
 		if opts.TerragruntConfigPath == "" {
 			opts.TerragruntConfigPath = config.GetDefaultConfigPath(opts.WorkingDir)
-		} else if ctx.Command.Name == terraform.CommandName {
+		} else if !filepath.IsAbs(opts.TerragruntConfigPath) && ctx.Command.Name == terraform.CommandName {
 			opts.TerragruntConfigPath = util.JoinPath(opts.WorkingDir, opts.TerragruntConfigPath)
 		}
 
