69e1e4ebddfe7b9005c9f09b3483070a0bab7868
Levko Burburas <62853952+levkohimins@users.noreply.github.com>
2023-09-18 22:44:25 +0300

    Removes trailing slash appended to the end of `get_path_to_repo_root()` (#2719)
    * based on reported issue #2158, removed the trailing slash and updated tests to match
    
    * chore: add integration test
    
    ---------
    
    Co-authored-by: Rush S <rush@r3-mpbm1.ru8733.local>

--- test/fixture-get-path/fixture-get-path-to-repo-root/main.tf
+++ test/fixture-get-path/fixture-get-path-to-repo-root/main.tf
@@ -2,6 +2,14 @@ variable "path_to_root" {
   type = string
 }
 
+variable "path_to_modules" {
+  type = string
+}
+
 output "path_to_root" {
   value = var.path_to_root
+}
+
+output "path_to_modules" {
+  value = var.path_to_modules
 }
