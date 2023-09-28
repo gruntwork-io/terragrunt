c58792cccc86134fd54c3e9f100eccf3bcd8220e
Denis O <denis.o@linux.com>
2023-09-28 11:21:46 -0700

    Add handling of empty state file

--- remote/terraform_state_file.go
+++ remote/terraform_state_file.go
@@ -62,19 +62,26 @@ func ParseTerraformStateFileFromLocation(backend string, config map[string]inter
 	}
 }
 
-// Parse the Terraform .tfstate file at the given path
+// ParseTerraformStateFile Parse the Terraform .tfstate file at the given path
 func ParseTerraformStateFile(path string) (*TerraformState, error) {
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, errors.WithStackTrace(CantParseTerraformStateFile{Path: path, UnderlyingErr: err})
 	}
-
-	return parseTerraformState(bytes)
+	state, err := parseTerraformState(bytes)
+	if err != nil {
+		return nil, errors.WithStackTrace(CantParseTerraformStateFile{Path: path, UnderlyingErr: err})
+	}
+	return state, nil
 }
 
 // Parse the Terraform state file data in the given byte slice
 func parseTerraformState(terraformStateData []byte) (*TerraformState, error) {
 	terraformState := &TerraformState{}
+
+	if len(terraformStateData) == 0 {
+		return terraformState, nil
+	}
 
 	if err := json.Unmarshal(terraformStateData, terraformState); err != nil {
 		return nil, errors.WithStackTrace(err)
