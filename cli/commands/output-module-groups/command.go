05303af90879d788c39e3ce7bd334bdd4e2dc404
Mason Cole <macole@beyondtrust.com>
2023-10-25 17:57:48 -0700

    fix!: output-module-groups to work like run-all
    output-module-groups was not working from a directory that did not have
    a terragrunt.hcl resource and it was not working with terragrunt cli options
    such as `terragrunt-modules-that-include`.
    
    furthermore, it was not inverting order for the destroy command.
    
    and required init even though it is just using the stackconfig to output
    planned behavior.
    
    this change aligns the behavior with run-all so that it will work with
    terragrunt cli options, properly display order based on apply and/or destory,
    and can be run withing any folder that run-all could be executed in.
    
    resolves: #2685
    resolves: #2768
    
    BREAKING CHANGE: technically this does change the behavior, in that it no longer requires terraform init. but i believe that more accurately reflects the desired behavior.

--- cli/commands/output-module-groups/command.go
+++ cli/commands/output-module-groups/command.go
@@ -6,13 +6,34 @@ import (
 )
 
 const (
-	CommandName = "output-module-groups"
+	CommandName       = "output-module-groups"
+	SubCommandApply   = "apply"
+	SubCommandDestroy = "destroy"
 )
 
 func NewCommand(opts *options.TerragruntOptions) *cli.Command {
 	return &cli.Command{
-		Name:   CommandName,
-		Usage:  "Output groups of modules ordered for apply as a list of list in JSON (useful for CI use cases).",
+		Name:        CommandName,
+		Usage:       "Output groups of modules ordered by command (apply or destroy) as a list of list in JSON (useful for CI use cases).",
+		Description: "The command will recursively find terragrunt modules in the current directory tree and output the list of list in JSON for the terraform command in dependency order (unless the command is destroy, in which case the command is run in reverse dependency order).",
+		Subcommands: subCommands(opts),
+		Action:      func(ctx *cli.Context) error { return Run(opts.OptionsFromContext(ctx)) },
+	}
+}
+
+func subCommands(opts *options.TerragruntOptions) cli.Commands {
+	cmds := cli.Commands{
+		subCommandFunc(SubCommandApply, opts),
+		subCommandFunc(SubCommandDestroy, opts),
+	}
+
+	return cmds
+}
+
+func subCommandFunc(cmd string, opts *options.TerragruntOptions) *cli.Command {
+	opts.TerraformCommand = cmd
+	return &cli.Command{
+		Name:   cmd,
 		Action: func(ctx *cli.Context) error { return Run(opts.OptionsFromContext(ctx)) },
 	}
 }
