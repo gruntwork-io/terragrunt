657093c727407e0477d7e2540fcd906cd936e80d
Mason Cole <macole@beyondtrust.com>
2023-10-26 06:57:07 -0700

    docs(output-module-groups): update docs and usage

--- cli/commands/output-module-groups/command.go
+++ cli/commands/output-module-groups/command.go
@@ -1,6 +1,8 @@
 package outputmodulegroups
 
 import (
+	"fmt"
+
 	"github.com/gruntwork-io/terragrunt/options"
 	"github.com/gruntwork-io/terragrunt/pkg/cli"
 )
@@ -15,7 +17,6 @@ func NewCommand(opts *options.TerragruntOptions) *cli.Command {
 	return &cli.Command{
 		Name:        CommandName,
 		Usage:       "Output groups of modules ordered by command (apply or destroy) as a list of list in JSON (useful for CI use cases).",
-		Description: "The command will recursively find terragrunt modules in the current directory tree and output the list of list in JSON for the terraform command in dependency order (unless the command is destroy, in which case the command is run in reverse dependency order).",
 		Subcommands: subCommands(opts),
 		Action:      func(ctx *cli.Context) error { return Run(opts.OptionsFromContext(ctx)) },
 	}
@@ -34,6 +35,7 @@ func subCommandFunc(cmd string, opts *options.TerragruntOptions) *cli.Command {
 	opts.TerraformCommand = cmd
 	return &cli.Command{
 		Name:   cmd,
+		Usage:  fmt.Sprintf("Recursively find terragrunt modules in the current directory tree and output the dependency order as a list of list in JSON for the %s", cmd),
 		Action: func(ctx *cli.Context) error { return Run(opts.OptionsFromContext(ctx)) },
 	}
 }
