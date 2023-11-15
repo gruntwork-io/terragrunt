33838c1f7ccad4821aeeec5e7b86cc98ed5a849f
Denis O <denis.o@linux.com>
2023-11-15 13:47:36 -0800

    Add running of boilerplate

--- cli/commands/scaffold/action.go
+++ cli/commands/scaffold/action.go
@@ -1,6 +1,11 @@
 package scaffold
 
 import (
+	"fmt"
+
+	boilerplate_options "github.com/gruntwork-io/boilerplate/options"
+	"github.com/gruntwork-io/boilerplate/templates"
+	"github.com/gruntwork-io/boilerplate/variables"
 	"github.com/gruntwork-io/terragrunt/options"
 	"github.com/gruntwork-io/terragrunt/test/fixture-download/invalid-path/.terragrunt-cache/JcmikJhv4-73PZ_MObPtv2y-Blk/p_piCTTWVab2Hmnj1OtnAruj8J4/errors"
 	"github.com/hashicorp/go-getter"
@@ -26,8 +31,18 @@ func Run(opts *options.TerragruntOptions) error {
 	}
 
 	// run boilerplate
+	opts.Logger.Infof("Running boilerplate in %s", opts.WorkingDir)
 
-	//boilerplate := cli.CreateBoilerplateCli()
+	bopts := &boilerplate_options.BoilerplateOptions{
+		TemplateUrl:  fmt.Sprintf("%s//boilerplate", opts.WorkingDir),
+		OutputFolder: opts.WorkingDir,
+	}
+
+	emptyDep := variables.Dependency{}
+	err = templates.ProcessTemplate(bopts, bopts, emptyDep)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
