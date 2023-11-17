21cb5c3b6a1fdfff31735533d88bdc2c091f4be0
Denis O <denis.o@linux.com>
2023-11-17 10:19:50 -0800

    add hcl parsing and variables extraction

--- cli/commands/scaffold/action.go
+++ cli/commands/scaffold/action.go
@@ -3,14 +3,19 @@ package scaffold
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/gruntwork-io/go-commons/errors"
 	"github.com/gruntwork-io/terratest/modules/files"
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 
 	boilerplate_options "github.com/gruntwork-io/boilerplate/options"
 	"github.com/gruntwork-io/boilerplate/templates"
 	"github.com/gruntwork-io/boilerplate/variables"
 	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/test/fixture-download/invalid-path/.terragrunt-cache/JcmikJhv4-73PZ_MObPtv2y-Blk/p_piCTTWVab2Hmnj1OtnAruj8J4/errors"
 	"github.com/hashicorp/go-getter"
 )
 
@@ -41,6 +46,9 @@ func Run(opts *options.TerragruntOptions) error {
 	if err != nil {
 		return errors.WithStackTrace(err)
 	}
+
+	listVariables(opts.WorkingDir)
+
 	// run boilerplate
 	opts.Logger.Infof("Running boilerplate in %s", opts.WorkingDir)
 
@@ -58,4 +66,62 @@ func Run(opts *options.TerragruntOptions) error {
 	}
 
 	return nil
+}
+
+func listVariables(directoryPath string) ([]string, error) {
+	tfFiles, err := listTerraformFiles(directoryPath)
+	if err != nil {
+		return nil, err
+	}
+	// Create an HCL parser
+	parser := hclparse.NewParser()
+
+	// Extract variables from all TF files
+	var allVariables []string
+	for _, tfFile := range tfFiles {
+		// Read the content of the TF file
+		content, err := ioutil.ReadFile(tfFile)
+		if err != nil {
+			log.Printf("Error reading file %s: %v", tfFile, err)
+			continue
+		}
+
+		// Parse the HCL content
+		file, diags := parser.ParseHCL(content, tfFile)
+		if diags.HasErrors() {
+			log.Printf("Failed to parse HCL in file %s: %v", tfFile, diags)
+			continue
+		}
+		if body, ok := file.Body.(*hclsyntax.Body); ok {
+			fmt.Sprintf("%v", body)
+			for _, block := range body.Blocks {
+				if block.Type == "variable" {
+					if len(block.Labels[0]) > 0 {
+						allVariables = append(allVariables, block.Labels[0])
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Printf("%v", allVariables)
+
+	return allVariables, nil
+}
+
+// listTerraformFiles returns a list of all TF files in the specified directory.
+func listTerraformFiles(directoryPath string) ([]string, error) {
+	var tfFiles []string
+
+	err := filepath.Walk(directoryPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".tf" {
+			tfFiles = append(tfFiles, path)
+		}
+		return nil
+	})
+
+	return tfFiles, err
 }
