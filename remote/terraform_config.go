1daf65580aea1525550c796749db2a7c49b387a2
Denis O <denis.o@linux.com>
2023-11-28 08:28:57 +0000

    Add handling of backend configs (#2810)

---
+++ remote/terraform_config.go
@@ -0,0 +1,30 @@
+package remote
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// wrapMapToSingleLineHcl - This is a workaround to convert a map[string]interface{} to a single line HCL string.
+func wrapMapToSingleLineHcl(m map[string]interface{}) string {
+	var attributes []string
+	for key, value := range m {
+		attributes = append(attributes, fmt.Sprintf(`%s=%s`, key, formatHclValue(value)))
+	}
+	sort.Strings(attributes)
+	return fmt.Sprintf("{%s}", strings.Join(attributes, ","))
+}
+
+// formatHclValue - Wrap single line HCL values in quotes.
+func formatHclValue(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		escapedValue := strings.ReplaceAll(v, `"`, `\"`)
+		return fmt.Sprintf(`"%s"`, escapedValue)
+	case map[string]interface{}:
+		return wrapMapToSingleLineHcl(v)
+	default:
+		return fmt.Sprintf(`%v`, v)
+	}
+}
