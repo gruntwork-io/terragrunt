52c58f2410a254b285692879f66acaed2a070763
Denis O <denis.o@linux.com>
2023-12-04 09:36:47 +0000

    Add external template test

--- test/integration_scaffold_test.go
+++ test/integration_scaffold_test.go
@@ -13,7 +13,7 @@ const (
 	// TODO: change ref values to master once the feature is merged
 	TEST_SCAFOLD_MODULE       = "https://github.com/gruntwork-io/terragrunt.git//test/fixture-scaffold/scaffold-module?ref=feature/scaffold"
 	TEST_SCAFOLD_MODULE_GIT   = "git@github.com:gruntwork-io/terragrunt.git//test/fixture-scaffold/scaffold-module?ref=feature/scaffold"
-	TEST_SCAFOLD_MODULE_SHORT = "github.com/gruntwork-io/terragrunt.git//test/fixture-inputs"
+	TEST_SCAFOLD_MODULE_SHORT = "github.com/gruntwork-io/terragrunt.git//test/fixture-inputs?ref=master"
 
 	TEST_SCAFOLD_TEMPLATE_MODULE          = "git@github.com:gruntwork-io/terragrunt.git//test/fixture-scaffold/module-with-template?ref=feature/scaffold"
 	TEST_SCAFOLD_EXTERNAL_TEMPLATE_MODULE = "git@github.com:gruntwork-io/terragrunt.git//test/fixture-scaffold/external-template?ref=feature/scaffold"
@@ -77,6 +77,23 @@ func TestTerragruntScaffoldModuleTemplate(t *testing.T) {
 	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold %s", tmpEnvPath, TEST_SCAFOLD_TEMPLATE_MODULE), &stdout, &stderr)
 	require.NoError(t, err)
 	require.Contains(t, stderr.String(), "Scaffolding completed")
-	// check that exists template file from .boilerplate dir
+	// check that exists file from .boilerplate dir
 	require.FileExists(t, fmt.Sprintf("%s/template-file.txt", tmpEnvPath))
+}
+
+func TestTerragruntScaffoldModuleExternalTemplate(t *testing.T) {
+	t.Parallel()
+
+	tmpEnvPath := t.TempDir()
+	err := os.MkdirAll(tmpEnvPath, 0755)
+	require.NoError(t, err)
+
+	stdout := bytes.Buffer{}
+	stderr := bytes.Buffer{}
+
+	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold %s %s", tmpEnvPath, TEST_SCAFOLD_MODULE_GIT, TEST_SCAFOLD_EXTERNAL_TEMPLATE_MODULE), &stdout, &stderr)
+	require.NoError(t, err)
+	require.Contains(t, stderr.String(), "Scaffolding completed")
+	// check that exists file from external template
+	require.FileExists(t, fmt.Sprintf("%s/external-template.txt", tmpEnvPath))
 }
