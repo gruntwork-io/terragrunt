4ab4cef3fdd5b6a513a62f2ce3e44bba84daebe2
Denis O <denis.o@linux.com>
2023-12-04 08:58:02 +0000

    Module with template

--- test/fixture-scaffold/scaffold-module/variables.tf
+++ test/fixture-scaffold/scaffold-module/variables.tf
@@ -1,16 +1,14 @@
 variable "project_name" {
   type        = string
   description = "Project name"
-
 }
-
 
 variable "vpc" {
   type        = string
   description = "VPC to be used"
   default     = "default-vpc"
-
 }
+
 variable "replica_count" {
   type    = number
   default = 666
