490d7a496bca19204cf6f7bcf9cf80481e599996
Denis O <denis.o@linux.com>
2023-12-06 19:40:03 +0000

    Add support for EnableRootInclude

--- test/integration_scaffold_test.go
+++ test/integration_scaffold_test.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/gruntwork-io/terragrunt/util"
 
 	"github.com/stretchr/testify/require"
 )
@@ -30,6 +33,7 @@ func TestTerragruntScaffoldModule(t *testing.T) {
 	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold %s", tmpEnvPath, TEST_SCAFOLD_MODULE), &stdout, &stderr)
 	require.NoError(t, err)
 	require.Contains(t, stderr.String(), "Scaffolding completed")
+	require.FileExists(t, fmt.Sprintf("%s/terragrunt.hcl", tmpEnvPath))
 }
 
 func TestTerragruntScaffoldModuleShortUrl(t *testing.T) {
@@ -44,6 +48,28 @@ func TestTerragruntScaffoldModuleShortUrl(t *testing.T) {
 	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold %s", tmpEnvPath, TEST_SCAFOLD_MODULE_SHORT), &stdout, &stderr)
 	require.NoError(t, err)
 	require.Contains(t, stderr.String(), "Scaffolding completed")
+	// check that find_in_parent_folders is generated in terragrunt.hcl
+	content, err := util.ReadFileAsString(fmt.Sprintf("%s/terragrunt.hcl", tmpEnvPath))
+	require.NoError(t, err)
+	require.Contains(t, content, "find_in_parent_folders")
+}
+
+func TestTerragruntScaffoldModuleShortUrlNoRootInclude(t *testing.T) {
+	t.Parallel()
+
+	tmpEnvPath, err := os.MkdirTemp("", "terragrunt-scaffold-test")
+	require.NoError(t, err)
+
+	stdout := bytes.Buffer{}
+	stderr := bytes.Buffer{}
+
+	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold %s --var=EnableRootInclude=false", tmpEnvPath, TEST_SCAFOLD_MODULE_SHORT), &stdout, &stderr)
+	require.NoError(t, err)
+	require.Contains(t, stderr.String(), "Scaffolding completed")
+	// check that find_in_parent_folders is NOT generated in  terragrunt.hcl
+	content, err := util.ReadFileAsString(fmt.Sprintf("%s/terragrunt.hcl", tmpEnvPath))
+	require.NoError(t, err)
+	require.NotContains(t, content, "find_in_parent_folders")
 }
 
 func TestTerragruntScaffoldModuleDifferentRevision(t *testing.T) {
@@ -135,4 +161,31 @@ func TestTerragruntScaffoldErrorNoModuleUrl(t *testing.T) {
 	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold", tmpEnvPath), &stdout, &stderr)
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "No module URL passed")
+}
+
+func TestTerragruntScaffoldModuleVarFile(t *testing.T) {
+	t.Parallel()
+	// generate var file with specific version, without root include and use GIT/SSH to clone module.
+	varFileContent := `
+Ref: v0.53.1
+EnableRootInclude: false
+SourceUrlType: "git-ssh"
+`
+	varFile := filepath.Join(t.TempDir(), "var-file.yaml")
+	err := os.WriteFile(varFile, []byte(varFileContent), 0644)
+	require.NoError(t, err)
+
+	tmpEnvPath, err := os.MkdirTemp("", "terragrunt-scaffold-test")
+	require.NoError(t, err)
+
+	stdout := bytes.Buffer{}
+	stderr := bytes.Buffer{}
+
+	err = runTerragruntCommand(t, fmt.Sprintf("terragrunt --terragrunt-non-interactive --terragrunt-working-dir %s scaffold %s --var-file=%s", tmpEnvPath, TEST_SCAFOLD_MODULE_SHORT, varFile), &stdout, &stderr)
+	require.NoError(t, err)
+	require.Contains(t, stderr.String(), "git::ssh://git@github.com/gruntwork-io/terragrunt.git//test/fixture-inputs?ref=v0.53.1")
+	require.Contains(t, stderr.String(), "Scaffolding completed")
+	content, err := util.ReadFileAsString(fmt.Sprintf("%s/terragrunt.hcl", tmpEnvPath))
+	require.NoError(t, err)
+	require.NotContains(t, content, "find_in_parent_folders")
 }
