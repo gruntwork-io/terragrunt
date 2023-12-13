eff629bf570abbc69c8d0360182ec4e4aeb27018
Denis O <denis.o@linux.com>
2023-12-13 20:05:31 +0000

    Add integration test for handling https urls

--- test/integration_catalog_test.go
+++ test/integration_catalog_test.go
@@ -54,16 +54,60 @@ func TestScaffoldGitModule(t *testing.T) {
 	err = cmd.Run()
 	assert.NoError(t, err)
 
-	assert.FileExists(t, fmt.Sprintf("%s/terragrunt.hcl", opts.WorkingDir))
-
-	opts, err = options.NewTerragruntOptionsForTest(filepath.Join(opts.WorkingDir, "terragrunt.hcl"))
-	assert.NoError(t, err)
-
-	cfg, err := config.ReadTerragruntConfig(opts)
-	assert.NoError(t, err)
+	cfg := readConfig(t, opts)
 	assert.NotEmpty(t, cfg.Inputs)
 	assert.Equal(t, 1, len(cfg.Inputs))
 	_, found := cfg.Inputs["vpc_id"]
 	assert.True(t, found)
 	assert.Contains(t, *cfg.Terraform.Source, "git::https://github.com/gruntwork-io/terraform-fake-modules.git//modules/aws/aurora")
+}
+
+func TestScaffoldGitModuleHttps(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	repo, err := module.NewRepo(ctx, "https://github.com/gruntwork-io/terraform-fake-modules")
+	assert.NoError(t, err)
+
+	modules, err := repo.FindModules(ctx)
+	assert.NoError(t, err)
+	var auroraModule *module.Module
+	for _, m := range modules {
+		if m.Title() == "Terraform Fake AWS Aurora Module" {
+			auroraModule = m
+		}
+	}
+	assert.NotNil(t, auroraModule)
+
+	testPath := t.TempDir()
+	opts, err := options.NewTerragruntOptionsForTest(testPath)
+	assert.NoError(t, err)
+
+	opts.ScaffoldVars = []string{"EnableRootInclude=false"}
+
+	cmd := command.NewScaffold(opts, auroraModule)
+	err = cmd.Run()
+	assert.NoError(t, err)
+
+	cfg := readConfig(t, opts)
+	assert.NotEmpty(t, cfg.Inputs)
+	assert.Equal(t, 1, len(cfg.Inputs))
+	_, found := cfg.Inputs["vpc_id"]
+	assert.True(t, found)
+	assert.Contains(t, *cfg.Terraform.Source, "git::https://github.com/gruntwork-io/terraform-fake-modules//modules/aws/aurora?ref=v0.0.5")
+
+	runTerragrunt(t, fmt.Sprintf("terragrunt plan --terragrunt-non-interactive --terragrunt-working-dir %s", opts.WorkingDir))
+}
+
+func readConfig(t *testing.T, opts *options.TerragruntOptions) *config.TerragruntConfig {
+	assert.FileExists(t, fmt.Sprintf("%s/terragrunt.hcl", opts.WorkingDir))
+
+	opts, err := options.NewTerragruntOptionsForTest(filepath.Join(opts.WorkingDir, "terragrunt.hcl"))
+	assert.NoError(t, err)
+
+	cfg, err := config.ReadTerragruntConfig(opts)
+	assert.NoError(t, err)
+
+	return cfg
 }
