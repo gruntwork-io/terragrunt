e14dbc92221bce5779647b0ad229fec0e976a814
Denis O <denis.o@linux.com>
2023-12-18 14:54:59 +0000

    passing assume role

--- codegen/generate.go
+++ codegen/generate.go
@@ -9,6 +9,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/hashicorp/hcl/v2/hclsimple"
+
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	ctyjson "github.com/zclconf/go-cty/cty/json"
 
@@ -45,10 +47,6 @@ const (
 
 	assumeRoleConfigKey = "assume_role"
 )
-
-var assumeRoleKeys = map[string]string{
-	"assume_role": "role_arn",
-}
 
 // Configuration for generating code
 type GenerateConfig struct {
@@ -171,25 +169,30 @@ func RemoteStateConfigToTerraformCode(backend string, config map[string]interfac
 		// Since we don't have the cty type information for the config and since config can be arbitrary, we cheat by using
 		// json as an intermediate representation.
 
-		_, isAssumeRole := config[assumeRoleConfigKey]
-		if isAssumeRole {
-			// fetch map from assume_role key
-			assumeRoleMap, _ := config[assumeRoleConfigKey].(map[string]interface{})
-			var keys []string
-			for key := range assumeRoleKeys {
-				keys = append(keys, key)
+		// handle assume role config key in a different way since it is a single line HCL object
+		if key == assumeRoleConfigKey {
+			assumeRoleValue, isAssumeRole := config[assumeRoleConfigKey].(string)
+			if !isAssumeRole {
+				continue
 			}
-			sort.Strings(keys)
-			for _, key := range keys {
-				roleValue, ok := assumeRoleMap[assumeRoleKeys[key]]
-				if ok {
-					ctyVal, err := convertValue(roleValue)
-					if err != nil {
-						return nil, errors.WithStackTrace(err)
-					}
-					backendBlockBody.SetAttributeValue(key, ctyVal.Value)
-				}
+			// extract assume role hcl values to be rendered in HCL
+			var assumeRoleMap map[string]string
+			// split single line hcl to default multiline file
+			hclValue := strings.TrimSuffix(assumeRoleValue, "}")
+			hclValue = strings.TrimPrefix(hclValue, "{")
+			hclValue = strings.ReplaceAll(hclValue, ",", "\n")
+			// basic decode of hcl to a map
+			err := hclsimple.Decode("s3_assume_role.hcl", []byte(hclValue), nil, &assumeRoleMap)
+			if err != nil {
+				return nil, errors.WithStackTrace(err)
 			}
+			// write assume role map as HCL object
+			ctyVal, err := convertValue(assumeRoleMap)
+			if err != nil {
+				return nil, errors.WithStackTrace(err)
+			}
+			backendBlockBody.SetAttributeValue(key, ctyVal.Value)
+			continue
 		}
 		ctyVal, err := convertValue(config[key])
 		if err != nil {
