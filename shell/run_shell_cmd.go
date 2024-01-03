63de551fae210381f320411ef8126a8e9db11757
Denis O <denis.o@linux.com>
2024-01-03 13:03:18 -0800

    Cleanup

--- shell/run_shell_cmd.go
+++ shell/run_shell_cmd.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
-	"path/filepath"
 	"strings"
 	"sync"
 	"syscall"
@@ -96,8 +95,7 @@ func RunShellCommandWithOutput(
 		defer terraformInitMutex.Unlock()
 		terraformInitMutex.Lock()
 	}
-	// transform path to native
-	command = filepath.Join(command)
+
 	terragruntOptions.Logger.Debugf("Running command: %s %s", command, strings.Join(args, " "))
 	if suppressStdout {
 		terragruntOptions.Logger.Debugf("Command output will be suppressed.")
