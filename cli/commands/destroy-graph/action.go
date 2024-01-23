457c1c1515b7ab45b13c5c8fe44e317ef0a0827b
Denis O <denis.o@linux.com>
2024-01-23 20:37:12 +0000

    Root path fixing

--- cli/commands/destroy-graph/action.go
+++ cli/commands/destroy-graph/action.go
@@ -3,28 +3,31 @@ package destroy_graph
 import (
 	"fmt"
 
-	"github.com/gruntwork-io/terragrunt/config"
 	"github.com/gruntwork-io/terragrunt/configstack"
 	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/gruntwork-io/terragrunt/shell"
 )
 
 func Run(opts *options.TerragruntOptions) error {
 
-	terragruntConfig, err := config.ReadTerragruntConfig(opts)
+	rootDir, err := shell.GitTopLevelDir(opts, opts.WorkingDir)
 	if err != nil {
 		return err
 	}
-	modules := configstack.FindWhereWorkingDirIsIncluded(opts, terragruntConfig)
 
-	if _, err := opts.ErrWriter.Write([]byte("Detected dependent modules:\n")); err != nil {
-		opts.Logger.Error(err)
+	rootOptions, err := options.NewTerragruntOptionsForTest(rootDir)
+	if err != nil {
 		return err
 	}
-	for _, module := range modules {
-		if _, err := opts.ErrWriter.Write([]byte(fmt.Sprintf("%s\n", module.Path))); err != nil {
-			opts.Logger.Error(err)
-			return err
-		}
+	stack, err := configstack.FindStackInSubfolders(rootOptions, nil)
+	if err != nil {
+		return err
 	}
+
+	fmt.Printf("%v\n", stack)
+
+	fmt.Printf("modules: \n%v\n", stack.Modules)
+
 	return nil
+
 }
