cd349cc41c3c739b34c5dc9edd5f8352a7089e78
Rob Morgan <robbym@gmail.com>
2024-03-12 13:15:33 +0800

    simplify the way we instantiate the tui

--- cli/commands/catalog/tui/tui.go
+++ cli/commands/catalog/tui/tui.go
@@ -2,27 +2,19 @@ package tui
 
 import (
 	"context"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gruntwork-io/terragrunt/cli/commands/catalog/module"
-	"github.com/gruntwork-io/terragrunt/cli/commands/catalog/tui/models/list"
 	"github.com/gruntwork-io/terragrunt/options"
 )
 
 func Run(ctx context.Context, modules module.Modules, opts *options.TerragruntOptions) error {
-	ctx, cancel := context.WithCancelCause(ctx)
-	quitFn := func(err error) {
-		go cancel(err)
-		if err != nil {
-			// explicit exit from application
-			os.Exit(1)
-		}
+	model, err := newModel(modules, opts)
+	if err != nil {
+		return err
 	}
 
-	list := list.NewModel(modules, quitFn, opts)
-
-	if _, err := tea.NewProgram(list, tea.WithAltScreen(), tea.WithContext(ctx)).Run(); err != nil {
+	if _, err := tea.NewProgram(model, tea.WithAltScreen(), tea.WithContext(ctx)).Run(); err != nil {
 		if err := context.Cause(ctx); err == context.Canceled {
 			return nil
 		} else if err != nil {
