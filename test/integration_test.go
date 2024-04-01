ce76bd13d07384b462e18701cccba68a8cf3b2b3
Denis O <denis.o@linux.com>
2024-04-01 19:11:09 +0100

    Added skip of tests not compatible with 1.5.x

--- test/integration_test.go
+++ test/integration_test.go
@@ -6377,6 +6377,10 @@ func TestTerragruntSkipConfirmExternalDependencies(t *testing.T) {
 
 func TestTerragruntInvokeTerraformTests(t *testing.T) {
 	t.Parallel()
+	if isTerraform() {
+		t.Skip("Not compatible with Terraform 1.5.x")
+		return
+	}
 
 	tmpEnvPath := copyEnvironment(t, TEST_FIXTURE_TF_TEST)
 	cleanupTerraformFolder(t, tmpEnvPath)
@@ -6672,6 +6676,10 @@ func TestTerragruntSkipDependenciesWithSkipFlag(t *testing.T) {
 
 func TestTerragruntAssumeRoleDuration(t *testing.T) {
 	t.Parallel()
+	if isTerraform() {
+		t.Skip("New assume role duration config not supported by Terraform 1.5.x")
+		return
+	}
 
 	tmpEnvPath := copyEnvironment(t, TEST_FIXTURE_ASSUME_ROLE_DURATION)
 	cleanupTerraformFolder(t, tmpEnvPath)
@@ -6745,4 +6753,12 @@ func wrappedBinary() string {
 		return TOFU_BINARY
 	}
 	return value
+}
+
+func isTerraform() bool {
+	return wrappedBinary() == TERRAFORM_BINARY
+}
+
+func isOpentofu() bool {
+	return wrappedBinary() == TOFU_BINARY
 }
