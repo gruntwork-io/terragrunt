7d36284a21745e1829f2d4e29f0d67bfdb8fc029
Levko Burburas <levko.himins@biptec.com>
2024-04-01 16:47:54 +0300

    fix: test

--- test/integration_test.go
+++ test/integration_test.go
@@ -6416,10 +6416,12 @@ func TestRenderJsonDependentModulesMetadataTerraform(t *testing.T) {
 func TestTerragruntSkipConfirmExternalDependencies(t *testing.T) {
 	// This test cannot be run using Terragrunt Provider Cache because it causes the flock files to be locked forever, which in turn blocks other TGs (processes).
 	// We use flock files to prevent multiple TGs from caching the same provider in parallel in a shared cache, which causes to conflicts.
-	providerCache, err := strconv.ParseBool(os.Getenv(commands.EnvVarNameTerragruntProviderCache))
-	require.NoError(t, err)
-	if providerCache {
-		return
+	if envProviderCache := os.Getenv(commands.EnvVarNameTerragruntProviderCache); envProviderCache != "" {
+		providerCache, err := strconv.ParseBool(envProviderCache)
+		require.NoError(t, err)
+		if providerCache {
+			return
+		}
 	}
 
 	t.Parallel()
