e879b6f43857f2104d0cd00f6ce1d4e26bb70894
Levko Burburas <levko.himins@biptec.com>
2024-04-09 12:03:51 +0300

    fix: test

--- test/integration_test.go
+++ test/integration_test.go
@@ -209,7 +209,7 @@ func TestTerragruntProviderCache(t *testing.T) {
 
 	cacheDir, err := util.GetCacheDir()
 	assert.NoError(t, err)
-	providerCacheDir := filepath.Join(cacheDir, "terragrunt-provider-cache-test")
+	providerCacheDir := filepath.Join(cacheDir, "provider-cache-test")
 
 	runTerragrunt(t, fmt.Sprintf("terragrunt run-all init --terragrunt-provider-cache --terragrunt-provider-cache-dir %s --terragrunt-log-level debug --terragrunt-non-interactive --terragrunt-working-dir %s", providerCacheDir, rootPath))
 
