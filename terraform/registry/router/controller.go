1cdc89a259a87e3e8335ed2bc298fa016851d531
Levko Burburas <levko.himins@biptec.com>
2024-04-10 20:02:18 +0300

    chore: remove artifacts

--- terraform/registry/router/controller.go
+++
@@ -1,8 +0,0 @@
-package router
-
-// Controller is an interface implemented by a REST controller
-type Controller interface {
-	// Register is the method called by the router, passing the router
-	// groups to let the controller register its methods
-	Register(router *Router)
-}
