9b65d7faccb11f229d07fcc49b5f09778830ff4a
Denis O <denis.o@linux.com>
2024-04-11 11:25:12 +0100

    Updated test path

--- test/integration_test.go
+++ test/integration_test.go
@@ -1150,7 +1150,7 @@ func testRemoteFixtureParallelism(t *testing.T, parallelism int, numberOfModules
 	s3BucketName := fmt.Sprintf("terragrunt-test-bucket-%s", strings.ToLower(uniqueId()))
 
 	// copy the template `numberOfModules` times into the app
-	tmpEnvPath, err := os.MkdirTemp("", fmt.Sprintf("terragrunt-test-%d", numberOfModules))
+	tmpEnvPath, err := os.MkdirTemp("", fmt.Sprintf("terragrunt-test-%d-", parallelism))
 	if err != nil {
 		t.Fatalf("Failed to create temp dir due to error: %v", err)
 	}
