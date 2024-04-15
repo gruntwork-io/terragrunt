b08e9e469f5fb68978817ab01dbcb09733f20c7a
Denis O <denis.o@linux.com>
2024-04-15 20:36:32 +0100

    Disabled json rendering

--- config/dependency.go
+++ config/dependency.go
@@ -630,7 +630,7 @@ func getTerragruntOutputJson(ctx *ParsingContext, targetConfig string) ([]byte, 
 	}
 	ctx = ctx.WithTerragruntOptions(targetTGOptions)
 
-	// targetTGOptions.TerraformLogsToJson = false
+	targetTGOptions.TerraformLogsToJson = false
 
 	// First attempt to parse the `remote_state` blocks without parsing/getting dependency outputs. If this is possible,
 	// proceed to routine that fetches remote state directly. Otherwise, fallback to calling `terragrunt output`
