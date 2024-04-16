bc345a1354d1d37a22bdbfccd160222a136ce9d3
Denis O <denis.o@linux.com>
2024-04-16 21:26:51 +0100

    Terragrunt options copy

--- config/dependency.go
+++ config/dependency.go
@@ -889,19 +889,13 @@ func runTerragruntOutputJson(ctx *ParsingContext, targetConfig string) ([]byte, 
 	// Update the stdout buffer so we can capture the output
 	var stdoutBuffer bytes.Buffer
 	stdoutBufferWriter := bufio.NewWriter(&stdoutBuffer)
-	ctx.TerragruntOptions.Writer = stdoutBufferWriter
 
-	currentTerraformLogsToJson := ctx.TerragruntOptions.TerraformLogsToJson
-	currentIncludeModulePrefix := ctx.TerragruntOptions.IncludeModulePrefix
-
-	// reset the settings after the run
-	defer func() {
-		ctx.TerragruntOptions.TerraformLogsToJson = currentTerraformLogsToJson
-		ctx.TerragruntOptions.IncludeModulePrefix = currentIncludeModulePrefix
-	}()
-	// Disable json wrapping to allow reading of outputs
-	ctx.TerragruntOptions.TerraformLogsToJson = false
-	ctx.TerragruntOptions.IncludeModulePrefix = false
+	newOpts := *ctx.TerragruntOptions
+	// explicit disable json formatting and prefixing to read json output
+	newOpts.TerraformLogsToJson = false
+	newOpts.IncludeModulePrefix = false
+	newOpts.Writer = stdoutBufferWriter
+	ctx = ctx.WithTerragruntOptions(&newOpts)
 
 	err := ctx.TerragruntOptions.RunTerragrunt(ctx, ctx.TerragruntOptions)
 	if err != nil {
