gruntwork-io/terragrunt

commit 0ecffe01e63b831593a8259d6560bd8e9fc9fa61

Add support for saving output plan (#3073)
* Add StackStateFolder

* Add passing of out folder

* Usage of output folder to store plans

* Add basic tests for out dir

* Common place for TerragruntOutDirFlagName

* Add Terragrunt out dir env variable

* Add file tests

* Added terraform save plan documentation

* Add CLI docs

* Output update

* FMT update

configstack/stack.go
@@ -6,8 +6,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"path/filepath"
 	"sort"
 	"strings"
+
+	"github.com/gruntwork-io/go-commons/collections"
 
 	"github.com/gruntwork-io/terragrunt/telemetry"
 	"github.com/gruntwork-io/terragrunt/terraform"
@@ -194,7 +197,19 @@ func FindStackInSubfolders(ctx context.Context, terragruntOptions *options.Terra
 // Sync the TerraformCliArgs for each module in the stack to match the provided terragruntOptions struct.
 func (stack *Stack) syncTerraformCliArgs(terragruntOptions *options.TerragruntOptions) {
 	for _, module := range stack.Modules {
-		module.TerragruntOptions.TerraformCliArgs = terragruntOptions.TerraformCliArgs
+		module.TerragruntOptions.TerraformCliArgs = collections.MakeCopyOfList(terragruntOptions.TerraformCliArgs)
+
+		// pass output location
+		if module.TerragruntOptions.OutputFolder != "" {
+			planFile := filepath.Join(module.TerragruntOptions.OutputFolder, util.FolderPathAsFile(module.Path)) + terraform.TerraformPlanFileExtension
+			terragruntOptions.Logger.Debugf("Using output file %s for module %s", planFile, module.TerragruntOptions.TerragruntConfigPath)
+			if module.TerragruntOptions.TerraformCommand == terraform.CommandNamePlan {
+				// for plan command add -out=<file> to the terraform cli args
+				module.TerragruntOptions.TerraformCliArgs = util.StringListInsert(module.TerragruntOptions.TerraformCliArgs, fmt.Sprintf("-out=%s", planFile), len(module.TerragruntOptions.TerraformCliArgs))
+			} else {
+				module.TerragruntOptions.TerraformCliArgs = util.StringListInsert(module.TerragruntOptions.TerraformCliArgs, planFile, len(module.TerragruntOptions.TerraformCliArgs))
+			}
+		}
 	}
 }
 

cli/commands/flags.go
@@ -43,6 +43,9 @@ const (
 	TerragruntDisableBucketUpdateFlagName            = "terragrunt-disable-bucket-update"
 	TerragruntDisableCommandValidationFlagName       = "terragrunt-disable-command-validation"
 
+	TerragruntOutDirFlagEnvVarName = "TERRAGRUNT_OUT_DIR"
+	TerragruntOutDirFlagName       = "terragrunt-out-dir"
+
 	// Terragrunt Provider Cache flags/envs
 	TerragruntProviderCacheFlagName                         = "terragrunt-provider-cache"
 	TerragruntProviderCacheEnvVarName                       = "TERRAGRUNT_PROVIDER_CACHE"

cli/commands/run-all/command.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"sort"
 
+	"github.com/gruntwork-io/terragrunt/cli/commands"
+
 	awsproviderpatch "github.com/gruntwork-io/terragrunt/cli/commands/aws-provider-patch"
 	graphdependencies "github.com/gruntwork-io/terragrunt/cli/commands/graph-dependencies"
 	"github.com/gruntwork-io/terragrunt/cli/commands/hclfmt"
@@ -26,6 +28,18 @@ func NewCommand(opts *options.TerragruntOptions) *cli.Command {
 		Description: "The command will recursively find terragrunt modules in the current directory tree and run the terraform command in dependency order (unless the command is destroy, in which case the command is run in reverse dependency order).",
 		Subcommands: subCommands(opts).SkipRunning(),
 		Action:      action(opts),
+		Flags:       NewFlags(opts).Sort(),
+	}
+}
+
+func NewFlags(opts *options.TerragruntOptions) cli.Flags {
+	return cli.Flags{
+		&cli.GenericFlag[string]{
+			Name:        commands.TerragruntOutDirFlagName,
+			EnvVar:      commands.TerragruntOutDirFlagEnvVarName,
+			Destination: &opts.OutputFolder,
+			Usage:       "Directory output files.",
+		},
 	}
 }
 

options/options.go
@@ -297,6 +297,9 @@ type TerragruntOptions struct {
 
 	// The list of remote registries to cached by Terragrunt Provider Cache server.
 	ProviderCacheRegistryNames []string
+
+	// Folder to store output files.
+	OutputFolder string
 }
 
 // IAMRoleOptions represents options that are used by Terragrunt to assume an IAM role.
@@ -376,6 +379,7 @@ func NewTerragruntOptions() *TerragruntOptions {
 			return errors.WithStackTrace(RunTerragruntCommandNotSet)
 		},
 		ProviderCacheRegistryNames: defaultProviderCacheRegistryNames,
+		OutputFolder:               "",
 	}
 }
 
@@ -509,6 +513,7 @@ func (opts *TerragruntOptions) Clone(terragruntConfigPath string) *TerragruntOpt
 		ProviderCacheArchiveDir:             opts.ProviderCacheArchiveDir,
 		ProviderCacheDisablePartialLockFile: opts.ProviderCacheDisablePartialLockFile,
 		DisableLogColors:                    opts.DisableLogColors,
+		OutputFolder:                        opts.OutputFolder,
 	}
 }
 

terraform/terraform.go
@@ -34,6 +34,8 @@ const (
 	EnvNameTFVarFmt                                = "TF_VAR_%s"
 
 	TerraformLockFile = ".terraform.lock.hcl"
+
+	TerraformPlanFileExtension = ".tfplan"
 )
 
 // ModuleVariables will return all the variables defined in the downloaded terraform modules, taking into

test/fixture-out-dir/app/main.tf
@@ -1,0 +1,10 @@
+variable "input_value" {}
+
+output "output_value" {
+  value = var.input_value
+}
+
+resource "local_file" "app_file" {
+  content  = "app file"
+  filename = "${path.module}/app_file.txt"
+}

test/fixture-out-dir/app/terragrunt.hcl
@@ -1,0 +1,13 @@
+
+dependency "dependency" {
+  config_path = "../dependency"
+
+  mock_outputs = {
+    result = "46521694"
+  }
+  mock_outputs_allowed_terraform_commands = ["plan", "apply"]
+}
+
+inputs = {
+  input_value = dependency.dependency.outputs.result
+}

test/fixture-out-dir/dependency/main.tf
@@ -1,0 +1,9 @@
+resource "local_file" "file" {
+  content  = "dependency file"
+  filename = "${path.module}/dependency_file.txt"
+}
+
+output "result" {
+
+  value = "42"
+}

commit 34c8fd63f8c191f4f7fc5fc9cc926de3d7a74bb5

fix: sync Terraform CLI args for 'show' command
In order to pass the plan filename.

Fixes: 0ecffe01e63b831593a8259d6560bd8e9fc9fa61

configstack/stack.go
@@ -126,6 +126,8 @@ func (stack *Stack) Run(ctx context.Context, terragruntOptions *options.Terragru
 			terragruntOptions.TerraformCliArgs = util.StringListInsert(terragruntOptions.TerraformCliArgs, "-auto-approve", 1)
 		}
 		stack.syncTerraformCliArgs(terragruntOptions)
+	case terraform.CommandNameShow:
+		stack.syncTerraformCliArgs(terragruntOptions)
 	case terraform.CommandNamePlan:
 		// We capture the out stream for each module
 		errorStreams := make([]bytes.Buffer, len(stack.Modules))