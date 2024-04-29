bdc02109f7c98d6d5e28b080e2872083343986d5
Levko Burburas <levko.himins@biptec.com>
2024-04-29 18:25:11 +0300

    chore: comment update

--- terraform/getproviders/provider.go
+++ terraform/getproviders/provider.go
@@ -9,13 +9,15 @@ import (
 type Providers []Provider
 
 type Provider interface {
+	// Address returns a source address of the provider. e.g.: registry.terraform.io/hashicorp/aws
 	Address() string
 
+	// version returns a version of the provider. e.g.: 5.36.0
 	Version() string
 
+	// DocumentSHA256Sums returns a document with providers hashes for different platforms.
 	DocumentSHA256Sums(ctx context.Context) ([]byte, error)
 
-	Signature(ctx context.Context) ([]byte, error)
-
+	// PackageDir returns a directory with the unpacked provider.
 	PackageDir() string
 }
