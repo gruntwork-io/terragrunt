3a568b9296bea01fc625b4dae25ca74ba4cae438
Denis O <denis.o@linux.com>
2024-06-21 16:02:06 +0100

    Engine block parsing

--- config/engine.go
+++ config/engine.go
@@ -2,8 +2,8 @@ package config
 
 // EngineConfig represents the structure of the HCL data
 type EngineConfig struct {
-	Source  string                 `hcl:"source"`
-	Version string                 `hcl:"version"`
-	Type    string                 `hcl:"type"`
-	Meta    map[string]interface{} `hcl:"meta"`
+	Source  string                 `hcl:"source" cty:"source"`
+	Version string                 `hcl:"version" cty:"version"`
+	Type    string                 `hcl:"type" cty:"type"`
+	Meta    map[string]interface{} `hcl:"meta" cty:"meta"`
 }
