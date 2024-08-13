1441ccb79dca2c2f8eb521ab2b35fea583cf43e3
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-08-13 16:30:03 -0400

    fix: Fixing `TestParseTimestamp`

--- util/datetime_test.go
+++ util/datetime_test.go
@@ -20,7 +20,7 @@ func TestParseTimestamp(t *testing.T) {
 		{"2017-11-22T00:00:00Z", time.Date(2017, time.Month(11), 22, 0, 0, 0, 0, time.UTC), ""},
 		{"2017-11-22T01:00:00+01:00", time.Date(2017, time.Month(11), 22, 1, 0, 0, 0, time.FixedZone("", 3600)), ""},
 		{"bloop", time.Time{}, `not a valid RFC3339 timestamp: cannot use "bloop" as year`},
-		{"2017-11-22 00:00:00Z", time.Time{}, `not a valid RFC3339 timestamp: missing assertd time introducer 'T'`},
+		{"2017-11-22 00:00:00Z", time.Time{}, `not a valid RFC3339 timestamp: missing required time introducer 'T'`},
 	}
 
 	for _, testCase := range testCases {
