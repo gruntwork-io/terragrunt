246e4f756c03762a830c1df12dc75d445768d39e
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-08-21 08:36:45 -0400

    fix: Fixing dynamodb check

--- dynamodb/dynamo_lock_table_test.go
+++ dynamodb/dynamo_lock_table_test.go
@@ -103,8 +103,8 @@ func TestTableTagging(t *testing.T) {
 	withLockTableTagged(t, tags, func(tableName string, client *awsDynamodb.DynamoDB) {
 		assertCanWriteToTable(t, tableName, client)
 
-		t.Logf("Not worth immediately checking tags on dynamo table. Tags can take a while to show up. Trying in 1 second.")
-		time.Sleep(1 * time.Second)
+		t.Logf("Not worth immediately checking tags on dynamo table. Tags can take a while to show up. Checking after a short delay.")
+		time.Sleep(5 * time.Second)
 
 		assertTags(t, tags, tableName, client)
 
