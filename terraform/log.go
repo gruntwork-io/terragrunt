a5da8a2a3e3e73a35ac17665dadb58932c8a39c9
Levko Burburas <levko.himins@biptec.com>
2024-09-13 23:21:13 +0300

    fix: tflog time parse

--- terraform/log.go
+++ terraform/log.go
@@ -11,11 +11,8 @@ import (
 )
 
 var (
-	// logTimestampFormat is TF_LOG timestamp formats, may depend on OS.
-	logTimestampFormats = []string{
-		"2006-01-02T15:04:05.000-0700",
-		"2006-01-02T15:04:05.000Z",
-	}
+	// logTimestampFormat is TF_LOG timestamp formats.
+	logTimestampFormat = "2006-01-02T15:04:05.000Z0700"
 )
 
 var (
@@ -23,7 +20,7 @@ var (
 	//
 	// 2024-09-08T13:44:31.229+0300 [DEBUG] using github.com/zclconf/go-cty v1.14.3
 	// 2024-09-08T13:44:31.229+0300 [INFO]  Go runtime version: go1.22.1
-	tfLogTimeLevelMsgReg = regexp.MustCompile(`(?i)(^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}(?:Z|[-+]\d{4}))\s*\[(trace|debug|warn|info|error)\]\s*(.+\S)$`)
+	tfLogTimeLevelMsgReg = regexp.MustCompile(`(?i)(^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}\S*)\s*\[(trace|debug|warn|info|error)\]\s*(.+\S)$`)
 )
 
 func ParseLogFunc(msgPrefix string) writer.WriterParseFunc {
@@ -46,17 +43,8 @@ func ParseLog(msgPrefix, str string) (msg string, ptrTime *time.Time, ptrLevel *
 
 	timeStr, levelStr, msg := match[1], match[2], match[3]
 
-	var timestampFormat = ""
-
-	for _, format := range logTimestampFormats {
-		if len(timeStr) == len(format) {
-			timestampFormat = format
-			break
-		}
-	}
-
-	if timeStr != "" && timestampFormat != "" {
-		time, err := time.Parse(timestampFormat, timeStr)
+	if timeStr != "" {
+		time, err := time.Parse(logTimestampFormat, timeStr)
 		if err != nil {
 			return "", nil, nil, errors.WithStackTrace(err)
 		}
