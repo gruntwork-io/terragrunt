37943c156036c37c6044e0473c4c68baa63946ce
Levko Burburas <levko.himins@biptec.com>
2024-10-04 01:48:08 +0200

    fix: signal package

--- internal/os/signal/signal_unix.go
+++ internal/os/signal/signal_unix.go
@@ -9,7 +9,7 @@ import (
 )
 
 // InterruptSignal is an interrupt signal.
-const InterruptSignal = syscall.SIGINT
+var InterruptSignal = syscall.SIGINT
 
 // InterruptSignals contains a list of signals that are treated as interrupts.
 var InterruptSignals []os.Signal = []os.Signal{syscall.SIGTERM, syscall.SIGINT}
