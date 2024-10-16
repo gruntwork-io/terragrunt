2cab98fc1a9978121d102099770ab15d93c5d42c
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-10-16 11:24:28 -0400

    feat: Adding Strict Package (#3424)
    * WIP - feat: Adding strict package
    
    * feat: Adding strict package
    
    * feat: Removing default on/off control. It just complicates things.
    
    * feat: Documenting Strict Mode
    
    * fix: Addressing markdownlint errors
    
    * fix: Addressing lints
    
    * fix: Blindly do what Levko says to do
    
    * fix: Incorporate review feedback
    
    * fix: Fixing docs for redesigned implementation
    
    * fix: Fixing bug in implementation.
    
    * fix: Cleaning up strict implementation
    
    * fix: Markdown linting
    
    * fix: Revert change to README
    
    * fix: Adding integration tests

---
+++ test/integration_strict_test.go
@@ -0,0 +1,82 @@
+package test_test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terragrunt/internal/strict"
+	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStrictMode(t *testing.T) {
+	t.Parallel()
+
+	cleanupTerraformFolder(t, testFixtureEmptyState)
+
+	tc := []struct {
+		name           string
+		controls       []string
+		strictMode     bool
+		expectedStderr string
+		expectedError  error
+	}{
+		{
+			name:           "plan-all",
+			controls:       []string{},
+			strictMode:     false,
+			expectedStderr: "The `plan-all` command is deprecated and will be removed in a future version. Use `terragrunt run-all plan` instead.",
+			expectedError:  nil,
+		},
+		{
+			name:           "plan-all with plan-all strict control",
+			controls:       []string{"plan-all"},
+			strictMode:     false,
+			expectedStderr: "",
+			expectedError:  strict.StrictControls[strict.PlanAll].Error,
+		},
+		{
+			name:           "plan-all with multiple strict controls",
+			controls:       []string{"plan-all", "apply-all"},
+			strictMode:     false,
+			expectedStderr: "",
+			expectedError:  strict.StrictControls[strict.PlanAll].Error,
+		},
+		{
+			name:           "plan-all with strict mode",
+			controls:       []string{},
+			strictMode:     true,
+			expectedStderr: "",
+			expectedError:  strict.StrictControls[strict.PlanAll].Error,
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			tmpEnvPath := copyEnvironment(t, testFixtureEmptyState)
+			rootPath := util.JoinPath(tmpEnvPath, testFixtureEmptyState)
+
+			args := "--terragrunt-non-interactive --terragrunt-log-level debug --terragrunt-working-dir " + rootPath
+			if tt.strictMode {
+				args = "--strict-mode " + args
+			}
+
+			for _, control := range tt.controls {
+				args = " --strict-control " + control + " " + args
+			}
+
+			_, stderr, err := runTerragruntCommandWithOutput(t, "terragrunt plan-all "+args)
+
+			if tt.expectedError != nil {
+				require.Error(t, err)
+				require.ErrorIs(t, err, tt.expectedError)
+			} else {
+				require.NoError(t, err)
+			}
+
+			assert.Contains(t, stderr, tt.expectedStderr)
+		})
+	}
+}
