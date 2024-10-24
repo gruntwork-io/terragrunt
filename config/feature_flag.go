d31effa0712b5fab2d8fda99795ed3b975d2d524
Denis O <denis.o@linux.com>
2024-10-24 14:42:39 +0000

    Feature flags merging

--- config/feature_flag.go
+++ config/feature_flag.go
@@ -1,12 +1,25 @@
 package config
 
+import "github.com/zclconf/go-cty/cty"
+
 type FeatureFlags []FeatureFlag
 
 type FeatureFlag struct {
-	Name    string `hcl:"name,attr"`
-	Default *bool  `hcl:"enabled,attr" cty:"default"`
+	Name    string     `hcl:"name,attr"`
+	Default *cty.Value `hcl:"default,attr" cty:"default"`
 }
 
-// Clone
+// DeepMerge merges the source FeatureFlag into the target FeatureFlag.
+func (feature *FeatureFlag) DeepMerge(source FeatureFlag) error {
+	if source.Default == nil {
+		feature.Default = source.Default
+	} else {
+		updatedDefaults, err := deepMergeCtyMaps(*feature.Default, *source.Default)
+		if err != nil {
+			return err
+		}
 
-// Merge
+		feature.Default = updatedDefaults
+	}
+	return nil
+}
