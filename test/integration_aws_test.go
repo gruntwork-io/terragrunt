6e3d26daed37f6875b08419ead6a32cca6a35e63
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-11-12 15:58:17 -0500

    fix: Adding debug logs

--- test/integration_aws_test.go
+++ test/integration_aws_test.go
@@ -799,7 +799,7 @@ func TestAwsAssumeRoleWebIdentityFile(t *testing.T) {
 	stdout := bytes.Buffer{}
 	stderr := bytes.Buffer{}
 
-	err := helpers.RunTerragruntCommand(t, "terragrunt apply -auto-approve --terragrunt-non-interactive --terragrunt-working-dir "+testPath, &stdout, &stderr)
+	err := helpers.RunTerragruntCommand(t, "terragrunt apply -auto-approve --terragrunt-non-interactive --terragrunt-log-level debug --terragrunt-working-dir "+testPath, &stdout, &stderr)
 	require.NoError(t, err)
 
 	output := fmt.Sprintf("%s %s", stderr.String(), stdout.String())
@@ -826,7 +826,7 @@ func TestAwsAssumeRoleWebIdentityFlag(t *testing.T) {
 	token := os.Getenv("CIRCLE_OIDC_TOKEN_V2")
 	assert.NotEmpty(t, token)
 
-	helpers.RunTerragrunt(t, "terragrunt apply --terragrunt-non-interactive --terragrunt-working-dir "+tmpEnvPath+" --terragrunt-iam-role "+roleARN+" --terragrunt-iam-web-identity-token "+token)
+	helpers.RunTerragrunt(t, "terragrunt apply --terragrunt-non-interactive --terragrunt-log-level debug --terragrunt-working-dir "+tmpEnvPath+" --terragrunt-iam-role '"+roleARN+"' --terragrunt-iam-web-identity-token '"+token+"'")
 }
 
 // Regression testing for https://github.com/gruntwork-io/terragrunt/issues/906
