9562cdebc29c27123a392ede736b00eaa8457fbb
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-11-18 09:33:08 -0500

    fix: Addressing true positives in tflint test

--- test/fixtures/tflint/module-found/dummy_module/main.tf
+++ test/fixtures/tflint/module-found/dummy_module/main.tf
@@ -1,1 +1,11 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.3"
+    }
+  }
+}
+
 resource "null_resource" "dummy" {}
