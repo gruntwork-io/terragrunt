1886158cb58743fe3f63fdbdbe5cb3e27978672c
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-11-18 15:01:55 -0500

    Revert "fix: Adding missing variables set by Terragrunt"
    This reverts commit f95e1373a1f51f9917f621947c9a8b2fbea3ecd4.

--- test/fixtures/tflint/module-found/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "The region in which the resources will be deployed"
-  default     = "us-west-2"
-}
-
-variable "environment" {
-  description = "The environment in which the resources will be deployed"
-  default     = "dev"
-}
