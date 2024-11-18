f95e1373a1f51f9917f621947c9a8b2fbea3ecd4
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-11-18 11:36:21 -0500

    fix: Adding missing variables set by Terragrunt

---
+++ test/fixtures/tflint/module-found/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "The region in which the resources will be deployed"
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "The environment in which the resources will be deployed"
+  default     = "dev"
+}
