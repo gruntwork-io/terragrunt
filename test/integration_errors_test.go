de2e878a38d4d3169d0d9b1b8e53f7d90c02363e
Denis O <denis.o@linux.com>
2024-12-01 11:34:40 +0000

    Run all ignore test

--- test/integration_errors_test.go
+++ test/integration_errors_test.go
@@ -11,8 +11,9 @@ import (
 )
 
 const (
-	testSimpleErrors = "fixtures/errors/default"
-	testIgnoreErrors = "fixtures/errors/ignore"
+	testSimpleErrors       = "fixtures/errors/default"
+	testIgnoreErrors       = "fixtures/errors/ignore"
+	testRunAllIgnoreErrors = "fixtures/errors/run-all-ignore"
 )
 
 func TestErrorsHandling(t *testing.T) {
@@ -34,9 +35,24 @@ func TestIgnoreError(t *testing.T) {
 	tmpEnvPath := helpers.CopyEnvironment(t, testIgnoreErrors)
 	rootPath := util.JoinPath(tmpEnvPath, testIgnoreErrors)
 
+	_, stderr, err := helpers.RunTerragruntCommandWithOutput(t, "terragrunt apply -auto-approve --terragrunt-non-interactive --terragrunt-working-dir "+rootPath)
+
+	require.NoError(t, err)
+	assert.Contains(t, stderr, "Ignoring error example1")
+	assert.NotContains(t, stderr, "Ignoring error example2")
+}
+
+func TestRunAllIgnoreError(t *testing.T) {
+	t.Parallel()
+
+	cleanupTerraformFolder(t, testRunAllIgnoreErrors)
+	tmpEnvPath := helpers.CopyEnvironment(t, testRunAllIgnoreErrors)
+	rootPath := util.JoinPath(tmpEnvPath, testRunAllIgnoreErrors)
+
 	_, stderr, err := helpers.RunTerragruntCommandWithOutput(t, "terragrunt run-all apply -auto-approve --terragrunt-non-interactive --terragrunt-working-dir "+rootPath)
 
 	require.NoError(t, err)
 	assert.Contains(t, stderr, "Ignoring error example1")
 	assert.NotContains(t, stderr, "Ignoring error example2")
+	assert.Contains(t, stderr, "value-from-app-2")
 }
