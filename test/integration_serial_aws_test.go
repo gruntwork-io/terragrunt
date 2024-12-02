d57c8942d826d3f8a72603a52d756dc744dec306
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-12-02 17:05:53 -0500

    fix: Removing unneeded config

--- test/integration_serial_aws_test.go
+++ test/integration_serial_aws_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/gruntwork-io/terragrunt/config"
 	"github.com/gruntwork-io/terragrunt/test/helpers"
 	"github.com/gruntwork-io/terragrunt/util"
 )
