b7530ca15e552dcd7d9f31ec42ec82e85fc3dd76
Denis O <denis.o@linux.com>
2024-12-06 19:30:31 +0000

    Add stack parsing

--- config/unit.go
+++ config/unit.go
@@ -1,5 +1,11 @@
 package config
 
+// StackConfigFile represents the structure of terragrunt stack file
+type StackConfigFile struct {
+	Locals *terragruntLocal `cty:"locals"  hcl:"locals,block"`
+	Units  []*Unit          `cty:"unit" hcl:"unit,block"`
+}
+
 // Unit represents a list of units.
 type Unit struct {
 	Name   string `cty:"name"    hcl:",label"`
