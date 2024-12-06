47cd72128c6035825ca52c3632fedff7ff4465c0
Denis O <denis.o@linux.com>
2024-12-06 19:34:48 +0000

    Add cty serialization

--- config/unit.go
+++ config/unit.go
@@ -1,6 +1,13 @@
 package config
 
-// StackConfigFile represents the structure of terragrunt stack file
+import (
+	"fmt"
+
+	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/gocty"
+)
+
+// StackConfigFile represents the structure of terragrunt.stack.hcl stack file
 type StackConfigFile struct {
 	Locals *terragruntLocal `cty:"locals"  hcl:"locals,block"`
 	Units  []*Unit          `cty:"unit" hcl:"unit,block"`
@@ -11,4 +18,28 @@ type Unit struct {
 	Name   string `cty:"name"    hcl:",label"`
 	Source string `hcl:"source,attr" cty:"source"`
 	Path   string `hcl:"source,attr" cty:"source"`
+}
+
+// ToCtyValue converts StackConfigFile to cty.Value
+func (s *StackConfigFile) ToCtyValue() (cty.Value, error) {
+	return gocty.ToCtyValue(s, cty.Object(map[string]cty.Type{
+		"locals": cty.Object(map[string]cty.Type{
+			// Define locals structure here
+		}),
+		"unit": cty.List(cty.Object(map[string]cty.Type{
+			"name":   cty.String,
+			"source": cty.String,
+			"path":   cty.String,
+		})),
+	}))
+}
+
+// FromCtyValue converts cty.Value back to StackConfigFile
+func FromCtyValue(v cty.Value) (*StackConfigFile, error) {
+	var config StackConfigFile
+	err := gocty.FromCtyValue(v, &config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode cty value: %w", err)
+	}
+	return &config, nil
 }
