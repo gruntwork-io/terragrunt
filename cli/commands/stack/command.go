fd83fb42b34613599d6b93d8160d80808ff5a591
Denis O <denis.o@linux.com>
2024-12-18 15:00:21 +0000

    Stack cli commands

--- cli/commands/stack/command.go
+++ cli/commands/stack/command.go
@@ -1,5 +1,27 @@
 package stack
 
+import (
+	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/gruntwork-io/terragrunt/pkg/cli"
+)
+
 const (
 	CommandName = "stack"
 )
+
+func NewFlags(opts *options.TerragruntOptions) cli.Flags {
+
+	return nil
+}
+
+func NewCommand(opts *options.TerragruntOptions) *cli.Command {
+	return &cli.Command{
+		Name:                   CommandName,
+		Usage:                  "Terragrunt stack commands.",
+		DisallowUndefinedFlags: true,
+		Flags:                  NewFlags(opts).Sort(),
+		Action: func(ctx *cli.Context) error {
+			return Run(ctx.Context, opts.OptionsFromContext(ctx))
+		},
+	}
+}
