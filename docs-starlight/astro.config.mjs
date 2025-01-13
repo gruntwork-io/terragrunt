4efb7db70e9c00a4ba959a7bc7957273b072fe99
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-01-13 11:47:25 -0500

    feat: Bare minimum starlight server as a starting point for prototyping

---
+++ docs-starlight/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Terragrunt',
+			social: {
+				github: 'https://github.com/gruntwork-io/terragrunt',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
