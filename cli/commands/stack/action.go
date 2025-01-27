098d22a69e678752177fbb83fdc3e5dbbef84ae4
Denis O <denis.o@linux.com>
2025-01-27 20:47:03 +0000

    stack output fetching

--- cli/commands/stack/action.go
+++ cli/commands/stack/action.go
@@ -69,6 +69,9 @@ func RunOutput(ctx context.Context, opts *options.TerragruntOptions) error {
 	opts.TerraformCommand = "output"
 	opts.OriginalTerraformCommand = "output"
 
+	if err := runall.Run(ctx, opts); err != nil {
+		return errors.New(err)
+	}
 	return nil
 }
 
