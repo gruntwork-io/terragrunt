81d61602ae5010c629c54c189aaa35f1f24e72c3
Denis O <denis.o@linux.com>
2025-01-30 19:56:24 +0000

    output fetching test

---
+++ test/fixtures/stacks/output/units/app1/main.tf
@@ -0,0 +1,7 @@
+output "data" {
+  value = "app1"
+}
+
+output "custom_value1" {
+  value = "value1"
+}
---
+++ test/fixtures/stacks/output/units/app2/main.tf
@@ -0,0 +1,7 @@
+output "data" {
+  value = "app2"
+}
+
+output "custom_value2" {
+  value = "value2"
+}
