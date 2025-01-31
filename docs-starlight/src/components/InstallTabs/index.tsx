b3e840ba65f0bf5713ce747c7d0f70b2e0d42b0b
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-01-31 10:08:23 -0500

    wip: WIP work on getting dynamic content

---
+++ docs-starlight/src/components/InstallTabs/index.tsx
@@ -0,0 +1,25 @@
+import { React } from 'react';
+import { Tabs, TabItem } from '@astrojs/starlight/components';
+import Install from '../Install';
+
+export default function InstallTabs({ version }) {
+	return (
+		<Tabs groupId="operating-systems">
+			<TabItem value="linux-amd64" label="Linux (x86)">
+				<Install os="linux" arch="amd64" version={version} />
+			</TabItem>
+			<TabItem value="macos-arm64" label="macOS (ARM)">
+				<Install os="darwin" arch="arm64" version={version} />
+			</TabItem>
+			<TabItem value="windows" label="Windows">
+				<Install os="windows" arch="amd64" version={version} />
+			</TabItem>
+			<TabItem value="linux-arm64" label="Linux (ARM)">
+				<Install os="linux" arch="arm64" version={version} />
+			</TabItem>
+			<TabItem value="macos-amd64" label="macOS (x86)">
+				<Install os="darwin" arch="amd64" version={version} />
+			</TabItem>
+		</Tabs>
+	);
+}
