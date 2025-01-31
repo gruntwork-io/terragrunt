920edc0c2127b8e29eab14715602f556b81560d0
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-01-31 17:16:31 -0500

    feat: Done prototyping

--- docs-starlight/src/content.config.ts
+++ docs-starlight/src/content.config.ts
@@ -1,7 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { glob } from 'astro/loaders';
 
-export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-};
+const docs = defineCollection({ loader: docsLoader(), schema: docsSchema() });
+const commands = defineCollection({
+	loader: glob({ pattern: "**/*.yml", base: "src/data/commands" }),
+	schema: z.object({
+		name: z.string(),
+		usage: z.string(),
+		description: z.string(),
+	}),
+});
+
+export const collections = { docs, commands };
