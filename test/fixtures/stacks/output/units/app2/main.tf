aaf583b58daa57c970375144b981c1cc62bc9cb9
Denis O <denis.o@linux.com>
2025-02-06 19:58:21 +0000

    test output update

--- test/fixtures/stacks/output/units/app2/main.tf
+++ test/fixtures/stacks/output/units/app2/main.tf
@@ -5,3 +5,33 @@ output "data" {
 output "custom_value2" {
   value = "value2"
 }
+
+output "complex" {
+  value = {
+    name      = "name2"
+    id        = 2
+    timestamp = timestamp()
+    delta     = 0.02
+  }
+}
+
+output "list" {
+  value = ["a", "b", "c"]
+}
+
+output "complex_list" {
+  value = [
+    {
+      name      = "name2"
+      id        = 2
+      timestamp = timestamp()
+      delta     = 0.02
+    },
+    {
+      name      = "name3"
+      id        = 2
+      timestamp = timestamp()
+      delta     = 0.03
+    }
+  ]
+}
