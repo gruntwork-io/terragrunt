023994ce83b13755567eb6103b03a247f3b0296d
Denis O <denis.o@linux.com>
2025-02-07 17:35:57 +0000

    Env vars printing

--- test/integration_destroy_test.go
+++ test/integration_destroy_test.go
@@ -327,9 +327,23 @@ func TestStorePlanFilesRunAllDestroy(t *testing.T) {
 	helpers.CleanupTerraformFolder(t, tmpEnvPath)
 	testPath := util.JoinPath(tmpEnvPath, testFixtureOutDir)
 
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		if strings.HasPrefix(pair[0], "TF_VAR_") {
+			fmt.Printf(" v1 %s=%s\n", pair[0], pair[1])
+		}
+	}
+
 	// plan and apply
 	_, _, err := helpers.RunTerragruntCommandWithOutput(t, fmt.Sprintf("terragrunt run-all plan --terragrunt-non-interactive --terragrunt-log-level trace --terragrunt-working-dir %s --terragrunt-out-dir %s", testPath, tmpDir))
 	require.NoError(t, err)
+
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		if strings.HasPrefix(pair[0], "TF_VAR_") {
+			fmt.Printf(" v2 %s=%s\n", pair[0], pair[1])
+		}
+	}
 
 	_, _, err = helpers.RunTerragruntCommandWithOutput(t, fmt.Sprintf("terragrunt run-all apply --terragrunt-non-interactive --terragrunt-log-level trace --terragrunt-working-dir %s --terragrunt-out-dir %s", testPath, tmpDir))
 	require.NoError(t, err)
@@ -340,6 +354,13 @@ func TestStorePlanFilesRunAllDestroy(t *testing.T) {
 	assert.Len(t, list, 2)
 	for _, file := range list {
 		assert.Equal(t, "tfplan.tfplan", filepath.Base(file))
+	}
+
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		if strings.HasPrefix(pair[0], "TF_VAR_") {
+			fmt.Printf(" v3 %s=%s\n", pair[0], pair[1])
+		}
 	}
 
 	// prepare destroy plan
@@ -353,6 +374,13 @@ func TestStorePlanFilesRunAllDestroy(t *testing.T) {
 	assert.Len(t, list, 2)
 	for _, file := range list {
 		assert.Equal(t, "tfplan.tfplan", filepath.Base(file))
+	}
+
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		if strings.HasPrefix(pair[0], "TF_VAR_") {
+			fmt.Printf(" v4 %s=%s\n", pair[0], pair[1])
+		}
 	}
 
 	_, _, err = helpers.RunTerragruntCommandWithOutput(t, fmt.Sprintf("terragrunt run-all apply --terragrunt-non-interactive --terragrunt-log-level trace --terragrunt-working-dir %s --terragrunt-out-dir %s", testPath, tmpDir))
