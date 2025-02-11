7b94c0f865040c976602ae17cf943df37085c247
Denis O <denis.o@linux.com>
2025-02-11 20:12:00 +0000

    Git config cleanup

--- _ci/install-golang.ps1
+++ _ci/install-golang.ps1
@@ -6,6 +6,8 @@ go version
 
 # configure git compression
 git config --global core.compression 0
+git config --global pack.threads "1"
+git config --global gc.auto 0
 
 try {
     # Enable Developer Mode
