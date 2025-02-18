1cb87c3dd22efe06229ea64b81810c4f625614f1
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-02-18 16:52:13 -0500

    feat: Working sidebar for commands

--- docs-starlight/astro.config.mjs
+++ docs-starlight/astro.config.mjs
@@ -3,6 +3,46 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
 
+export const sidebar = [
+	{
+		label: 'Getting Started',
+		autogenerate: { directory: '01-getting-started' },
+	},
+	{
+		label: 'Features',
+		autogenerate: { directory: '02-features', collapsed: true },
+	},
+	{
+		label: 'Community',
+		autogenerate: { directory: '03-community', collapsed: true },
+	},
+	{
+		label: 'Reference',
+		items: [
+			{ label: 'HCL', autogenerate: { directory: '04-reference/01-hcl', collapsed: true } },
+			{
+				label: 'CLI', collapsed: true, items: [
+					{ label: 'Overview', slug: 'docs/reference/cli' },
+					{ label: 'Rules', slug: 'docs/reference/cli/rules' },
+				],
+			},
+			{ label: 'Strict Controls', slug: 'docs/reference/strict-controls' },
+			{ label: 'Experiments', slug: 'docs/reference/experiments' },
+			{ label: 'Supported Versions', slug: 'docs/reference/supported-versions' },
+			{ label: 'Lock Files', slug: 'docs/reference/lock-files' },
+			{ label: 'Terragrunt Cache', slug: 'docs/reference/terragrunt-cache' },
+		],
+	},
+	{
+		label: 'Troubleshooting',
+		autogenerate: { directory: '05-troubleshooting', collapsed: true },
+	},
+	{
+		label: 'Migrate',
+		autogenerate: { directory: '06-migrate', collapsed: true },
+	},
+];
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
@@ -16,46 +56,7 @@ export default defineConfig({
 				github: 'https://github.com/gruntwork-io/terragrunt',
 				discord: 'https://discord.gg/SPu4Degs5f',
 			},
-			sidebar: [
-				{
-					label: 'Getting Started',
-					autogenerate: { directory: '01-getting-started' },
-				},
-				{
-					label: 'Features',
-					autogenerate: { directory: '02-features', collapsed: true },
-				},
-				{
-					label: 'Community',
-					autogenerate: { directory: '03-community', collapsed: true },
-				},
-				{
-					label: 'Reference',
-					items: [
-						{ label: 'HCL', autogenerate: { directory: '04-reference/01-hcl', collapsed: true } },
-						{
-							label: 'CLI', collapsed: true, items: [
-								{ label: 'Overview', slug: 'docs/reference/cli' },
-								{ label: 'Commands', autogenerate: { directory: '04-reference/02-cli/02-commands', collapsed: true } },
-								{ label: 'Rules', slug: 'docs/reference/cli/rules' },
-							],
-						},
-						{ label: 'Strict Controls', slug: 'docs/reference/strict-controls' },
-						{ label: 'Experiments', slug: 'docs/reference/experiments' },
-						{ label: 'Supported Versions', slug: 'docs/reference/supported-versions' },
-						{ label: 'Lock Files', slug: 'docs/reference/lock-files' },
-						{ label: 'Terragrunt Cache', slug: 'docs/reference/terragrunt-cache' },
-					],
-				},
-				{
-					label: 'Troubleshooting',
-					autogenerate: { directory: '05-troubleshooting', collapsed: true },
-				},
-				{
-					label: 'Migrate',
-					autogenerate: { directory: '06-migrate', collapsed: true },
-				},
-			],
+			sidebar: sidebar,
 			// NOTE: We don't currently check links by default because the CLI
 			// Redesign isn't done yet. Once those pages are built out, we'll require
 			// links to be checked for all builds.
