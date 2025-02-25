afde8a2bdde3d1b58974e91f9205a81e9ffc042c
juan-vg <juan.vela@lansweeper.com>
2025-02-25 13:48:00 +0100

    Create test case for the GetTargetVersion func

--- tf/getter_test.go
+++ tf/getter_test.go
@@ -153,5 +153,82 @@ func TestBuildRequestUrlRelativePath(t *testing.T) {
 	requestURL, err := tf.BuildRequestURL("gruntwork.io", "/registry/modules/v1", "/tfr-project/terraform-aws-tfr", "6.6.6")
 	require.NoError(t, err)
 	assert.Equal(t, "https://gruntwork.io/registry/modules/v1/tfr-project/terraform-aws-tfr/6.6.6/download", requestURL.String())
+}
 
+// Combine all the tests cases below into one test function, including the last one where a non existant module is requested
+func TestGetTargetVersion(t *testing.T) {
+	t.Parallel()
+	registryDomain := "registry.terraform.io"
+	moduleRegistryBasePath := "/v1/modules/"
+	tc := []struct {
+		name           string
+		modulePath     string
+		targetVersion  string
+		expectedResult string
+	}{
+		{
+			name:           "FixedVersion",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  "3.3.0",
+			expectedResult: "3.3.0",
+		},
+		{
+			name:           "PessimisticPatchVersion",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  "~> 0.0.1",
+			expectedResult: "0.0.7",
+		},
+		{
+			name:           "PessimisticMinorVersion",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  "~> 3.3",
+			expectedResult: "3.16.0",
+		},
+		{
+			name:           "ComplexConstraint",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  ">= 3.3.0, <5.0.0,!= 4.24.1,!=4.24.0",
+			expectedResult: "4.23.0",
+		},
+		{
+			name:           "InvalidConstraint",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  ">= 3.3.0 and >4.24.1",
+			expectedResult: "",
+		},
+		{
+			name:           "UnsatisfiableConstraint",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  ">= 3.3.0, <3.2.0",
+			expectedResult: "",
+		},
+		{
+			name:           "InvalidVersion",
+			modulePath:     "/terraform-aws-modules/iam/aws",
+			targetVersion:  "~> a.b.c",
+			expectedResult: "",
+		},
+		{
+			name:           "NotExistingModule",
+			modulePath:     "/terraform-not-existing-modules/not-existing-module",
+			targetVersion:  "3.3.0",
+			expectedResult: "",
+		},
+	}
+
+	for _, tt := range tc {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			targetVersion, err := tf.GetTargetVersion(context.Background(), log.New(), registryDomain, moduleRegistryBasePath, tt.modulePath, tt.targetVersion)
+			if tt.expectedResult == "" {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedResult, targetVersion)
+			}
+		})
+	}
 }
