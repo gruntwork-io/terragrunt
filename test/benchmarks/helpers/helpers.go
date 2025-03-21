6165616e4a0790c54ea45b82726a559defb3cc43
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-03-21 16:23:13 -0400

    feat: Reorganizing a bit

--- test/benchmarks/helpers/helpers.go
+++ test/benchmarks/helpers/helpers.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terragrunt/cli"
 	"github.com/gruntwork-io/terragrunt/options"
@@ -14,7 +15,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func RunTerragruntCommand(tb *testing.B, args ...string) {
+func RunTerragruntCommand(b *testing.B, args ...string) {
+	b.Helper()
+
 	writer := io.Discard
 	errwriter := io.Discard
 
@@ -24,7 +27,7 @@ func RunTerragruntCommand(tb *testing.B, args ...string) {
 	ctx := log.ContextWithLogger(context.Background(), opts.Logger)
 
 	err := app.RunContext(ctx, args)
-	require.NoError(tb, err)
+	require.NoError(b, err)
 }
 
 func GenerateNUnits(b *testing.B, tmpDir string, n int, tgConfig string, tfConfig string) {
@@ -42,4 +45,33 @@ func GenerateNUnits(b *testing.B, tmpDir string, n int, tgConfig string, tfConfi
 		unitMainTfPath := filepath.Join(unitDir, "main.tf")
 		require.NoError(b, os.WriteFile(unitMainTfPath, []byte(tfConfig), 0644))
 	}
+}
+
+func PlanApplyDestroy(b *testing.B, dir string) {
+	b.Helper()
+
+	// Measure plan time
+	planStart := time.Now()
+
+	RunTerragruntCommand(b, "terragrunt", "run-all", "plan", "--non-interactive", "--working-dir", dir)
+
+	planDuration := time.Since(planStart)
+
+	// Measure apply time
+	applyStart := time.Now()
+
+	RunTerragruntCommand(b, "terragrunt", "run-all", "apply", "-auto-approve", "--non-interactive", "--working-dir", dir)
+
+	applyDuration := time.Since(applyStart)
+
+	// Measure destroy time
+	destroyStart := time.Now()
+
+	RunTerragruntCommand(b, "terragrunt", "run-all", "destroy", "-auto-approve", "--non-interactive", "--working-dir", dir)
+
+	destroyDuration := time.Since(destroyStart)
+
+	b.ReportMetric(float64(planDuration.Seconds()), "plan_s/op")
+	b.ReportMetric(float64(applyDuration.Seconds()), "apply_s/op")
+	b.ReportMetric(float64(destroyDuration.Seconds()), "destroy_s/op")
 }
--- test/benchmarks/integration_bench_test.go
+++ test/benchmarks/integration_bench_test.go
@@ -4,15 +4,12 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/gruntwork-io/terragrunt/test/benchmarks/helpers"
 	"github.com/stretchr/testify/require"
 )
 
 func BenchmarkEmptyTerragruntPlanApply(b *testing.B) {
-
-	emptyTerragruntConfig := ``
 	emptyMainTf := ``
 
 	emptyRootConfig := ``
@@ -20,37 +17,6 @@ func BenchmarkEmptyTerragruntPlanApply(b *testing.B) {
 		path = find_in_parent_folders("root.hcl")
 }
 `
-
-	b.Run("1 unit", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			b.StopTimer()
-
-			// Create a temporary directory for each test
-			tmpDir := b.TempDir()
-			terragruntConfigPath := filepath.Join(tmpDir, "terragrunt.hcl")
-
-			// Create an empty `terragrunt.hcl` file
-			require.NoError(b, os.WriteFile(terragruntConfigPath, []byte(emptyTerragruntConfig), 0644))
-
-			// Create an empty `main.tf` file
-			mainTfPath := filepath.Join(tmpDir, "main.tf")
-			require.NoError(b, os.WriteFile(mainTfPath, []byte(emptyMainTf), 0644))
-			b.StartTimer()
-
-			// Measure plan time
-			planStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "plan", "--non-interactive", "--working-dir", tmpDir)
-			planDuration := time.Since(planStart)
-
-			// Measure apply time
-			applyStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "apply", "-auto-approve", "--non-interactive", "--working-dir", tmpDir)
-			applyDuration := time.Since(applyStart)
-
-			b.ReportMetric(float64(planDuration.Milliseconds()), "plan_ms/op")
-			b.ReportMetric(float64(applyDuration.Milliseconds()), "apply_ms/op")
-		}
-	})
 
 	b.Run("10 units", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
@@ -67,18 +33,7 @@ func BenchmarkEmptyTerragruntPlanApply(b *testing.B) {
 
 			b.StartTimer()
 
-			// Measure plan time
-			planStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "run-all", "plan", "--non-interactive", "--working-dir", tmpDir)
-			planDuration := time.Since(planStart)
-
-			// Measure apply time
-			applyStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "run-all", "apply", "-auto-approve", "--non-interactive", "--working-dir", tmpDir)
-			applyDuration := time.Since(applyStart)
-
-			b.ReportMetric(float64(planDuration.Milliseconds()), "plan_ms/op")
-			b.ReportMetric(float64(applyDuration.Milliseconds()), "apply_ms/op")
+			helpers.PlanApplyDestroy(b, tmpDir)
 		}
 	})
 
@@ -98,18 +53,7 @@ func BenchmarkEmptyTerragruntPlanApply(b *testing.B) {
 
 			b.StartTimer()
 
-			// Measure plan time
-			planStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "run-all", "plan", "--non-interactive", "--working-dir", tmpDir)
-			planDuration := time.Since(planStart)
-
-			// Measure apply time
-			applyStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "run-all", "apply", "-auto-approve", "--non-interactive", "--working-dir", tmpDir)
-			applyDuration := time.Since(applyStart)
-
-			b.ReportMetric(float64(planDuration.Milliseconds()), "plan_ms/op")
-			b.ReportMetric(float64(applyDuration.Milliseconds()), "apply_ms/op")
+			helpers.PlanApplyDestroy(b, tmpDir)
 		}
 	})
 
@@ -129,18 +73,7 @@ func BenchmarkEmptyTerragruntPlanApply(b *testing.B) {
 
 			b.StartTimer()
 
-			// Measure plan time
-			planStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "run-all", "plan", "--non-interactive", "--working-dir", tmpDir)
-			planDuration := time.Since(planStart)
-
-			// Measure apply time
-			applyStart := time.Now()
-			helpers.RunTerragruntCommand(b, "terragrunt", "run-all", "apply", "-auto-approve", "--non-interactive", "--working-dir", tmpDir)
-			applyDuration := time.Since(applyStart)
-
-			b.ReportMetric(float64(planDuration.Milliseconds()), "plan_ms/op")
-			b.ReportMetric(float64(applyDuration.Milliseconds()), "apply_ms/op")
+			helpers.PlanApplyDestroy(b, tmpDir)
 		}
 	})
 }
