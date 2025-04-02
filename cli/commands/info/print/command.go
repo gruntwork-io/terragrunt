2cdb0f08427d95bb91f9a471f8a13a70231c8e94
Denis O <denis.o@linux.com>
2025-04-02 21:35:53 +0100

    Add print command

---
+++ cli/commands/info/print/command.go
@@ -0,0 +1,32 @@
+package print
+
+import (
+	"github.com/gruntwork-io/terragrunt/cli/flags"
+	"github.com/gruntwork-io/terragrunt/internal/cli"
+	"github.com/gruntwork-io/terragrunt/options"
+)
+
+const (
+	CommandName = "print"
+)
+
+func NewListFlags(_ *options.TerragruntOptions, _ flags.Prefix) cli.Flags {
+	return cli.Flags{}
+}
+
+func NewCommand(opts *options.TerragruntOptions, prefix flags.Prefix) *cli.Command {
+	prefix = prefix.Append(CommandName)
+
+	return &cli.Command{
+		Name:  CommandName,
+		Usage: "Print out a short description of Terragrunt context.",
+
+		ErrorOnUndefinedFlag: true,
+		Action:               infoAction(opts),
+	}
+}
+
+func infoAction(opts *options.TerragruntOptions) func(ctx *cli.Context) error {
+
+	return nil
+}
