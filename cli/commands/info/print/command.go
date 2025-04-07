45d4665f98933cfd36725ca6d1367ac27ba82549
Denis O <denis.o@linux.com>
2025-04-07 21:57:10 +0100

    add print command

--- cli/commands/info/print/command.go
+++ cli/commands/info/print/command.go
@@ -1,6 +1,9 @@
 package print
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/gruntwork-io/terragrunt/cli/flags"
 	"github.com/gruntwork-io/terragrunt/internal/cli"
 	"github.com/gruntwork-io/terragrunt/options"
@@ -9,6 +12,16 @@ import (
 const (
 	CommandName = "print"
 )
+
+// InfoOutput represents the structured output of the info command
+type InfoOutput struct {
+	ConfigPath       string `json:"config_path"`
+	DownloadDir      string `json:"download_dir"`
+	IAMRole          string `json:"iam_role"`
+	TerraformBinary  string `json:"terraform_binary"`
+	TerraformCommand string `json:"terraform_command"`
+	WorkingDir       string `json:"working_dir"`
+}
 
 func NewListFlags(_ *options.TerragruntOptions, _ flags.Prefix) cli.Flags {
 	return cli.Flags{}
@@ -28,6 +41,26 @@ func NewCommand(opts *options.TerragruntOptions, prefix flags.Prefix) *cli.Comma
 }
 
 func infoAction(opts *options.TerragruntOptions) func(ctx *cli.Context) error {
+	return func(c *cli.Context) error {
+		output := InfoOutput{
+			ConfigPath:       opts.TerragruntConfigPath,
+			DownloadDir:      opts.DownloadDir,
+			IAMRole:          opts.IAMRoleOptions.RoleARN,
+			TerraformBinary:  opts.TerraformPath,
+			TerraformCommand: opts.TerraformCommand,
+			WorkingDir:       opts.WorkingDir,
+		}
 
-	return nil
+		jsonBytes, err := json.MarshalIndent(output, "", "  ")
+		if err != nil {
+			opts.Logger.Error("failed to marshal info output to JSON")
+			return fmt.Errorf("failed to marshal info output: %w", err)
+		}
+
+		if _, err := fmt.Fprintln(opts.Writer, string(jsonBytes)); err != nil {
+			return fmt.Errorf("failed to write info output: %w", err)
+		}
+
+		return nil
+	}
 }
