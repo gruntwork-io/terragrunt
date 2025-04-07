140a535e3b9cc735a34b74594bc9c0d7a552bd4d
Denis O <denis.o@linux.com>
2025-04-07 22:06:28 +0100

    Info printing

--- cli/commands/info/print/command.go
+++ cli/commands/info/print/command.go
@@ -1,8 +1,13 @@
 package print
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+
+	"github.com/gruntwork-io/terragrunt/cli/commands/run"
+	"github.com/gruntwork-io/terragrunt/config"
+	"github.com/gruntwork-io/terragrunt/internal/errors"
 
 	"github.com/gruntwork-io/terragrunt/cli/flags"
 	"github.com/gruntwork-io/terragrunt/internal/cli"
@@ -36,31 +41,48 @@ func NewCommand(opts *options.TerragruntOptions, prefix flags.Prefix) *cli.Comma
 		UsageText:            "terragrunt info print",
 		Flags:                NewListFlags(opts, prefix),
 		ErrorOnUndefinedFlag: true,
-		Action:               infoAction(opts),
+		Action: func(ctx *cli.Context) error {
+			target := run.NewTargetWithErrorHandler(run.TargetPointDownloadSource, runInfo, runErrorInfo)
+
+			return run.RunWithTarget(ctx, opts, target)
+		},
 	}
 }
 
-func infoAction(opts *options.TerragruntOptions) func(ctx *cli.Context) error {
-	return func(c *cli.Context) error {
-		output := InfoOutput{
-			ConfigPath:       opts.TerragruntConfigPath,
-			DownloadDir:      opts.DownloadDir,
-			IAMRole:          opts.IAMRoleOptions.RoleARN,
-			TerraformBinary:  opts.TerraformPath,
-			TerraformCommand: opts.TerraformCommand,
-			WorkingDir:       opts.WorkingDir,
-		}
-
-		jsonBytes, err := json.MarshalIndent(output, "", "  ")
-		if err != nil {
-			opts.Logger.Error("failed to marshal info output to JSON")
-			return fmt.Errorf("failed to marshal info output: %w", err)
-		}
-
-		if _, err := fmt.Fprintln(opts.Writer, string(jsonBytes)); err != nil {
-			return fmt.Errorf("failed to write info output: %w", err)
-		}
-
-		return nil
+func printInfo(opts *options.TerragruntOptions) error {
+	group := InfoOutput{
+		ConfigPath:       opts.TerragruntConfigPath,
+		DownloadDir:      opts.DownloadDir,
+		IAMRole:          opts.IAMRoleOptions.RoleARN,
+		TerraformBinary:  opts.TerraformPath,
+		TerraformCommand: opts.TerraformCommand,
+		WorkingDir:       opts.WorkingDir,
 	}
+
+	b, err := json.MarshalIndent(group, "", "  ")
+	if err != nil {
+		opts.Logger.Errorf("JSON error marshalling info")
+
+		return errors.New(err)
+	}
+
+	if _, err := fmt.Fprintf(opts.Writer, "%s\n", b); err != nil {
+		return errors.New(err)
+	}
+
+	return nil
+}
+
+func runInfo(_ context.Context, opts *options.TerragruntOptions, _ *config.TerragruntConfig) error {
+	return printInfo(opts)
+}
+
+func runErrorInfo(opts *options.TerragruntOptions, _ *config.TerragruntConfig, err error) error {
+	opts.Logger.Debugf("Fetching info: %v", err)
+
+	if err := printInfo(opts); err != nil {
+		opts.Logger.Errorf("Error printing info: %v", err)
+	}
+
+	return nil
 }
