81a05a3a407c04d69970b05cfecf0c5cdd9e9938
Denis O <denis.o@linux.com>
2025-04-10 20:09:17 +0100

    Add dag graph command

--- cli/commands/dag/graph/command.go
+++ cli/commands/dag/graph/command.go
@@ -1,8 +1,10 @@
 package graph
 
 import (
-	"github.com/gruntwork-io/terragrunt/cli/commands/run"
+	"github.com/gruntwork-io/terragrunt/cli/commands/common/graph"
+	"github.com/gruntwork-io/terragrunt/cli/commands/common/runall"
 	"github.com/gruntwork-io/terragrunt/cli/flags"
+	"github.com/gruntwork-io/terragrunt/configstack"
 	"github.com/gruntwork-io/terragrunt/internal/cli"
 	"github.com/gruntwork-io/terragrunt/options"
 )
@@ -11,21 +13,32 @@ const (
 	CommandName = "graph"
 )
 
-func NewListFlags(opts *options.TerragruntOptions, prefix flags.Prefix) cli.Flags {
-	return run.NewFlags(opts, prefix)
-}
-
 func NewCommand(opts *options.TerragruntOptions, prefix flags.Prefix) *cli.Command {
 	prefix = prefix.Append(CommandName)
 
-	return &cli.Command{
+	cmd := &cli.Command{
 		Name:                 CommandName,
 		Usage:                "Graph the Directed Acyclic Graph (DAG) in DOT language.",
 		UsageText:            "terragrunt dag graph",
-		Flags:                NewListFlags(opts, prefix),
 		ErrorOnUndefinedFlag: true,
 		Action: func(ctx *cli.Context) error {
-			return nil
+			return Run(ctx, opts)
 		},
 	}
+
+	cmd = runall.WrapCommand(opts, cmd)
+	cmd = graph.WrapCommand(opts, cmd)
+
+	return cmd
+}
+
+func Run(ctx *cli.Context, opts *options.TerragruntOptions) error {
+	stack, err := configstack.FindStackInSubfolders(ctx, opts)
+	if err != nil {
+		return err
+	}
+
+	stack.Graph(opts)
+
+	return nil
 }
