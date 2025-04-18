2f7fc4d660da417dddacc45902a5c90334f4faff
Denis O <denis.o@linux.com>
2025-04-18 14:42:20 +0100

    Add usage of experiment function

--- internal/discovery/discovery.go
+++ internal/discovery/discovery.go
@@ -4,11 +4,13 @@ package discovery
 import (
 	"context"
 	"io"
-	"io/fs"
 	"os"
 	"path/filepath"
 	"sort"
 	"strings"
+
+	"github.com/gruntwork-io/terragrunt/internal/experiment"
+	"github.com/gruntwork-io/terragrunt/util"
 
 	"github.com/gruntwork-io/terragrunt/telemetry"
 
@@ -272,49 +274,53 @@ func (d *Discovery) isInHiddenDirectory(path string) bool {
 func (d *Discovery) Discover(ctx context.Context, opts *options.TerragruntOptions) (DiscoveredConfigs, error) {
 	var cfgs DiscoveredConfigs
 
-	walkFn :=
-		func(path string, e fs.DirEntry, err error) error {
-			if err != nil {
-				return errors.New(err)
-			}
+	processFn := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return errors.New(err)
+		}
 
-			if e.IsDir() {
-				return nil
-			}
-
-			if !d.hidden && d.isInHiddenDirectory(path) {
-				return nil
-			}
-
-			switch filepath.Base(path) {
-			case config.DefaultTerragruntConfigPath:
-				cfg := &DiscoveredConfig{
-					Type: ConfigTypeUnit,
-					Path: filepath.Dir(path),
-				}
-
-				if d.discoveryContext != nil {
-					cfg.DiscoveryContext = d.discoveryContext
-				}
-
-				cfgs = append(cfgs, cfg)
-			case config.DefaultStackFile:
-				cfg := &DiscoveredConfig{
-					Type: ConfigTypeStack,
-					Path: filepath.Dir(path),
-				}
-
-				if d.discoveryContext != nil {
-					cfg.DiscoveryContext = d.discoveryContext
-				}
-
-				cfgs = append(cfgs, cfg)
-			}
-
+		if info.IsDir() {
 			return nil
 		}
 
-	if err := filepath.WalkDir(d.workingDir, walkFn); err != nil {
+		if !d.hidden && d.isInHiddenDirectory(path) {
+			return nil
+		}
+
+		switch filepath.Base(path) {
+		case config.DefaultTerragruntConfigPath:
+			cfg := &DiscoveredConfig{
+				Type: ConfigTypeUnit,
+				Path: filepath.Dir(path),
+			}
+
+			if d.discoveryContext != nil {
+				cfg.DiscoveryContext = d.discoveryContext
+			}
+
+			cfgs = append(cfgs, cfg)
+		case config.DefaultStackFile:
+			cfg := &DiscoveredConfig{
+				Type: ConfigTypeStack,
+				Path: filepath.Dir(path),
+			}
+
+			if d.discoveryContext != nil {
+				cfg.DiscoveryContext = d.discoveryContext
+			}
+
+			cfgs = append(cfgs, cfg)
+		}
+
+		return nil
+	}
+
+	walkFn := filepath.Walk
+	if opts.Experiments.Evaluate(experiment.Symlinks) {
+		walkFn = util.WalkWithSymlinks
+	}
+
+	if err := walkFn(d.workingDir, processFn); err != nil {
 		return cfgs, errors.New(err)
 	}
 
