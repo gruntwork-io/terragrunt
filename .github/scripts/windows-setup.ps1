599efe41687898143b85c65e2e5de7a1cd515f6a
Denis O <denis.o@linux.com>
2025-04-23 15:40:31 +0300

    Add bath setup for windows cicd

--- .github/scripts/windows-setup.ps1
+++ .github/scripts/windows-setup.ps1
@@ -15,3 +15,5 @@ mkdir C:\bin
 cmd /c mklink C:\bin\sh.exe "C:\Program Files\Git\usr\bin\bash.exe"
 cmd /c mklink C:\bin\bash.exe "C:\Program Files\Git\usr\bin\bash.exe"
 echo "C:\bin" | Out-File -Append -FilePath $env:GITHUB_PATH
+echo "C:\bin" | Out-File -Append -FilePath $env:PATH
+echo "C:\Program Files\Git\usr\bin" | Out-File -Append -FilePath $env:PATH
