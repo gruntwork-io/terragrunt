aa12ceb1a49ee0502eff9ef78a3d27fab6571268
Denis O <denis.o@linux.com>
2025-05-05 17:54:40 +0100

    add engine download step

--- .github/scripts/setup/engine.sh
+++ .github/scripts/setup/engine.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -euo pipefail
-export TOFU_ENGINE_VERSION: "v0.0.16"
+export TOFU_ENGINE_VERSION="v0.0.16"
 export REPO="gruntwork-io/terragrunt-engine-opentofu"
 export ASSET_NAME="terragrunt-iac-engine-opentofu_rpc_${TOFU_ENGINE_VERSION}_linux_amd64.zip"
+pushd .
+# Download the engine binary
+mkdir -p /tmp/engine
+cd /tmp/engine
 wget -O "engine.zip" "https://github.com/${REPO}/releases/download/${TOFU_ENGINE_VERSION}/${ASSET_NAME}"
 unzip -o "engine.zip"
+popd
