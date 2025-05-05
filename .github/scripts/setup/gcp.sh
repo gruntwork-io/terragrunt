623bacf5ae41063150c381b4c8af50ca19a18eea
Denis O <denis.o@linux.com>
2025-05-05 15:57:45 +0100

    Add auth config

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
-echo 'export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json' >> $BASH_ENV
+#echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
+#echo 'export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json' >> $BASH_ENV
