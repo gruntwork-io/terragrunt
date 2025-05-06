5be046aabb1b9180ed8da026e23406a29ef1823b
Denis O <denis.o@linux.com>
2025-05-06 20:11:04 +0100

    GCP cleanup

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 echo "$GCLOUD_SERVICE_KEY" > "${HOME}/gcloud-service-key.json"
+ls -lahrt ${HOME}/gcloud-service-key.json
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 
+gcloud auth activate-service-account --key-file="${HOME}/gcloud-service-key.json"
+gcloud auth application-default login --quiet --impersonate-service-account $(gcloud auth list --filter="status:ACTIVE" --format="value(account)")
 gcloud config set project "${GOOGLE_PROJECT_ID}"
