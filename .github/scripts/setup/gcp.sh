730d5a7d2299d2c09688ddfdd4fe2f207002c3fc
Denis O <denis.o@linux.com>
2025-05-06 09:16:08 +0100

    gcp setting

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -euo pipefail
+echo "$GCLOUD_SERVICE_KEY" > "${HOME}/gcloud-service-key.json"
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 
 gcloud config set project "${GOOGLE_PROJECT_ID}"
