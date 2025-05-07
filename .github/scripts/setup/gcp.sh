2763f12dfbfe0f822eb59ce0e156c36f1cae942b
Denis O <denis.o@linux.com>
2025-05-07 16:41:48 +0100

    Setup GCP cli

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 echo "$GCLOUD_SERVICE_KEY" > "${HOME}/gcloud-service-key.json"
-ls -lahrt ${HOME}/gcloud-service-key.json
-file ${HOME}/gcloud-service-key.json
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 
+# Set up gcloud CLI
 gcloud auth activate-service-account --key-file="${HOME}/gcloud-service-key.json" --quiet
 gcloud config set project "${GOOGLE_PROJECT_ID}"
