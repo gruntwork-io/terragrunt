7fb3557ed305ba49a790a53020952712e8e5c391
Denis O <denis.o@linux.com>
2025-05-07 16:51:20 +0100

    Add GOOGLE_APPLICATION_CREDENTIALS saving in env file

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 echo "$GCLOUD_SERVICE_KEY" > "${HOME}/gcloud-service-key.json"
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
+printf "export GOOGLE_APPLICATION_CREDENTIALS='%s'\n" "${HOME}/gcloud-service-key.json" >> "$ENV_FILE"
 
 # Set up gcloud CLI
 gcloud auth activate-service-account --key-file="${HOME}/gcloud-service-key.json" --quiet
