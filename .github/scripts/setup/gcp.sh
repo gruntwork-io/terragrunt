3fcd436742aee87fc1956f9ace251973d391d823
Denis O <denis.o@linux.com>
2025-05-07 16:52:21 +0100

    Added env file saving

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -euo pipefail
+
+: "${ENV_FILE:?ENV_FILE is not set}"
+
+touch "$ENV_FILE"
+
 echo "$GCLOUD_SERVICE_KEY" > "${HOME}/gcloud-service-key.json"
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 printf "export GOOGLE_APPLICATION_CREDENTIALS='%s'\n" "${HOME}/gcloud-service-key.json" >> "$ENV_FILE"
