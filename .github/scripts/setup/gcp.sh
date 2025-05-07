a7dc8047bf3cf1e38c5bba01af74c2abbd79a12b
Denis O <denis.o@linux.com>
2025-05-07 18:10:33 +0100

    Project id update

--- .github/scripts/setup/gcp.sh
+++ .github/scripts/setup/gcp.sh
@@ -9,6 +9,7 @@ touch "$ENV_FILE"
 echo "$GCLOUD_SERVICE_KEY" > "${HOME}/gcloud-service-key.json"
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 printf "export GOOGLE_APPLICATION_CREDENTIALS='%s'\n" "${HOME}/gcloud-service-key.json" >> "$ENV_FILE"
+printf "export GOOGLE_PROJECT_ID='%s'\n" "terragrunt-458620" >> "$ENV_FILE"
 
 # Set up gcloud CLI
 gcloud auth activate-service-account --key-file="${HOME}/gcloud-service-key.json" --quiet
