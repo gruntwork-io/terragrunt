27c73b3dab4fb138d0ef8631ac754fdd0bc97c1d
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-05-21 17:54:57 -0400

    feat: Adding cache for eval context

--- config/context.go
+++ config/context.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gruntwork-io/terragrunt/config/hclparse"
 	"github.com/gruntwork-io/terragrunt/internal/cache"
+	"github.com/hashicorp/hcl/v2"
 )
 
 type configKey byte
@@ -12,11 +13,13 @@ type configKey byte
 const (
 	HclCacheContextKey              configKey = iota
 	TerragruntConfigCacheContextKey configKey = iota
+	EvalCtxCacheContextKey          configKey = iota
 	RunCmdCacheContextKey           configKey = iota
 	DependencyOutputCacheContextKey configKey = iota
 
 	hclCacheName              = "hclCache"
 	configCacheName           = "configCache"
+	evalCtxCacheName          = "evalCtxCache"
 	runCmdCacheName           = "runCmdCache"
 	dependencyOutputCacheName = "dependencyOutputCache"
 )
@@ -25,6 +28,7 @@ const (
 func WithConfigValues(ctx context.Context) context.Context {
 	ctx = context.WithValue(ctx, HclCacheContextKey, cache.NewCache[*hclparse.File](hclCacheName))
 	ctx = context.WithValue(ctx, TerragruntConfigCacheContextKey, cache.NewCache[*TerragruntConfig](configCacheName))
+	ctx = context.WithValue(ctx, EvalCtxCacheContextKey, cache.NewCache[*hcl.EvalContext](evalCtxCacheName))
 	ctx = context.WithValue(ctx, RunCmdCacheContextKey, cache.NewCache[string](runCmdCacheName))
 	ctx = context.WithValue(ctx, DependencyOutputCacheContextKey, cache.NewCache[*dependencyOutputCache](dependencyOutputCacheName))
 
