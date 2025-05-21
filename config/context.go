e117c5f0dbc2d697e7d10439cd5bb3c0195c3649
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-05-21 18:23:44 -0400

    feat: Adding opts cache

--- config/context.go
+++ config/context.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gruntwork-io/terragrunt/config/hclparse"
 	"github.com/gruntwork-io/terragrunt/internal/cache"
+	"github.com/gruntwork-io/terragrunt/options"
 	"github.com/hashicorp/hcl/v2"
 )
 
@@ -16,12 +17,14 @@ const (
 	EvalCtxCacheContextKey          configKey = iota
 	RunCmdCacheContextKey           configKey = iota
 	DependencyOutputCacheContextKey configKey = iota
+	OptsCacheContextKey             configKey = iota
 
 	hclCacheName              = "hclCache"
 	configCacheName           = "configCache"
 	evalCtxCacheName          = "evalCtxCache"
 	runCmdCacheName           = "runCmdCache"
 	dependencyOutputCacheName = "dependencyOutputCache"
+	optsCacheName             = "optsCache"
 )
 
 // WithConfigValues add to context default values for configuration.
@@ -31,6 +34,7 @@ func WithConfigValues(ctx context.Context) context.Context {
 	ctx = context.WithValue(ctx, EvalCtxCacheContextKey, cache.NewCache[*hcl.EvalContext](evalCtxCacheName))
 	ctx = context.WithValue(ctx, RunCmdCacheContextKey, cache.NewCache[string](runCmdCacheName))
 	ctx = context.WithValue(ctx, DependencyOutputCacheContextKey, cache.NewCache[*dependencyOutputCache](dependencyOutputCacheName))
+	ctx = context.WithValue(ctx, OptsCacheContextKey, cache.NewCache[*options.TerragruntOptions](optsCacheName))
 
 	return ctx
 }
