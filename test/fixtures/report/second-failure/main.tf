74fd891d47e6a1343bbad54c578815222c066e47
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-06-04 16:07:14 -0400

    fix: Fixing pre-commit errors

--- test/fixtures/report/second-failure/main.tf
+++ test/fixtures/report/second-failure/main.tf
@@ -1,1 +1,18 @@
-Crash!
+terraform {
+  backend "s3" {}
+
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
+
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "exit 1"
+  }
+}
