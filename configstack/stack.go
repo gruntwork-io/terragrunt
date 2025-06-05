aa0f33efb7b57101ee8c425099401aaaa4eb42c2
Denis O <denis.o@linux.com>
2025-06-05 19:57:16 +0100

    Add runner pool stack

--- configstack/stack.go
+++ configstack/stack.go
@@ -9,7 +9,9 @@ import (
 	"github.com/gruntwork-io/terragrunt/pkg/log"
 
 	"github.com/gruntwork-io/terragrunt/config"
+	"github.com/gruntwork-io/terragrunt/discovery"
 	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/gruntwork-io/terragrunt/queue"
 )
 
 // Stack is the abstraction for a stack of Terraform modules.
@@ -34,6 +36,36 @@ type Stack interface {
 // StackBuilder is the abstraction for building a Stack.
 type StackBuilder interface {
 	BuildStack(ctx context.Context, l log.Logger, terragruntOptions *options.TerragruntOptions, opts ...Option) (Stack, error)
+}
+
+// RunnerPoolStackBuilder implements StackBuilder for RunnerPoolStack
+// This is a new builder that uses discovery and queue for abstract handling of run --all/--graph
+// Not yet wired into FindStackInSubfolders.
+type RunnerPoolStackBuilder struct{}
+
+// BuildStack builds a new RunnerPoolStack using discovery and queue
+func (b *RunnerPoolStackBuilder) BuildStack(ctx context.Context, l log.Logger, terragruntOptions *options.TerragruntOptions, opts ...Option) (Stack, error) {
+	d := discovery.NewDiscovery(terragruntOptions.WorkingDir).
+		WithSuppressParseErrors().
+		WithDiscoverDependencies().
+		WithDiscoverExternalDependencies()
+
+	discovered, err := d.Discover(ctx, l, terragruntOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	q, err := queue.NewQueue(discovered)
+	if err != nil {
+		return nil, err
+	}
+
+	stack := NewRunnerPoolStack(discovered, q, terragruntOptions)
+	for _, opt := range opts {
+		opt(stack)
+	}
+
+	return stack, nil
 }
 
 // FindStackInSubfolders finds all the Terraform modules in the subfolders of the working directory of the given TerragruntOptions and
