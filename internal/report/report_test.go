7243c6c0852b6009fa3b1037249ef0308cdcb624
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-06-05 16:13:39 -0400

    fix: Updating testing for CSV generation

--- internal/report/report_test.go
+++ internal/report/report_test.go
@@ -264,8 +264,6 @@ func TestSummarize(t *testing.T) {
 func TestWriteCSV(t *testing.T) {
 	t.Parallel()
 
-	tmp := t.TempDir()
-
 	tests := []struct {
 		name     string
 		setup    func(dir string, r *report.Report)
