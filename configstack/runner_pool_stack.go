269cd9d05087884ec6d40863c1c0e031b4ad3344
Denis O <denis.o@linux.com>
2025-06-11 17:17:25 +0100

    Runner pool fix

--- configstack/runner_pool_stack.go
+++ configstack/runner_pool_stack.go
@@ -2,6 +2,12 @@ package configstack
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 	"sync"
 
 	"github.com/gruntwork-io/terragrunt/util"
@@ -25,10 +31,10 @@ type RunnerPoolStack struct {
 }
 
 // NewRunnerPoolStack creates a new stack from discovered modules.
-func NewRunnerPoolStack(ctx context.Context, l log.Logger, terragruntOptions *options.TerragruntOptions, discovered discovery.DiscoveredConfigs) (*DefaultStack, error) {
+func NewRunnerPoolStack(ctx context.Context, l log.Logger, terragruntOptions *options.TerragruntOptions, discovered discovery.DiscoveredConfigs) (*RunnerPoolStack, error) {
 	modulesMap := make(TerraformModulesMap)
 
-	stack := &DefaultStack{
+	stack := &RunnerPoolStack{
 		terragruntOptions: terragruntOptions,
 		parserOptions:     config.DefaultParserOptions(l, terragruntOptions),
 	}
@@ -70,4 +76,177 @@ func NewRunnerPoolStack(ctx context.Context, l log.Logger, terragruntOptions *op
 	stack.modules = linkedModules
 
 	return stack, nil
+}
+
+func (stack *RunnerPoolStack) SetReport(report *report.Report) {
+	stack.report = report
+}
+
+func (stack *RunnerPoolStack) GetReport() *report.Report {
+	return stack.report
+}
+
+func (stack *RunnerPoolStack) String() string {
+	modules := []string{}
+	for _, module := range stack.modules {
+		modules = append(modules, "  => "+module.String())
+	}
+	// Sort for deterministic output
+	sort.Strings(modules)
+	return fmt.Sprintf("Stack at %s:\n%s", stack.terragruntOptions.WorkingDir, strings.Join(modules, "\n"))
+}
+
+func (stack *RunnerPoolStack) LogModuleDeployOrder(l log.Logger, terraformCommand string) error {
+	outStr := fmt.Sprintf("The stack at %s will be processed in the following order for command %s:\n", stack.terragruntOptions.WorkingDir, terraformCommand)
+	runGraph, err := stack.GetModuleRunGraph(terraformCommand)
+	if err != nil {
+		return err
+	}
+	for i, group := range runGraph {
+		outStr += fmt.Sprintf("Group %d\n", i+1)
+		for _, module := range group {
+			outStr += fmt.Sprintf("- Module %s\n", module.Path)
+		}
+		outStr += "\n"
+	}
+	l.Info(outStr)
+	return nil
+}
+
+func (stack *RunnerPoolStack) JSONModuleDeployOrder(terraformCommand string) (string, error) {
+	runGraph, err := stack.GetModuleRunGraph(terraformCommand)
+	if err != nil {
+		return "", err
+	}
+	jsonGraph := make(map[string][]string)
+	for i, group := range runGraph {
+		groupNum := fmt.Sprintf("Group %d", i+1)
+		jsonGraph[groupNum] = make([]string, len(group))
+		for j, module := range group {
+			jsonGraph[groupNum][j] = module.Path
+		}
+	}
+	j, err := json.MarshalIndent(jsonGraph, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(j), nil
+}
+
+func (stack *RunnerPoolStack) Graph(l log.Logger, opts *options.TerragruntOptions) {
+	err := stack.modules.WriteDot(l, opts.Writer, opts)
+	if err != nil {
+		l.Warnf("Failed to graph dot: %v", err)
+	}
+}
+
+func (stack *RunnerPoolStack) Run(ctx context.Context, l log.Logger, opts *options.TerragruntOptions) error {
+	stackCmd := opts.TerraformCommand
+	if opts.OutputFolder != "" {
+		for _, module := range stack.modules {
+			planFile := module.outputFile(l, opts)
+			planDir := filepath.Dir(planFile)
+			if err := os.MkdirAll(planDir, os.ModePerm); err != nil {
+				return err
+			}
+		}
+	}
+	if util.ListContainsElement(config.TerraformCommandsNeedInput, stackCmd) {
+		opts.TerraformCliArgs = util.StringListInsert(opts.TerraformCliArgs, "-input=false", 1)
+		// No syncTerraformCliArgs for runner pool, but can be added if needed
+	}
+	switch {
+	case opts.IgnoreDependencyOrder:
+		return stack.modules.RunModulesIgnoreOrder(ctx, opts, stack.report, opts.Parallelism)
+	case stackCmd == "destroy":
+		return stack.modules.RunModulesReverseOrder(ctx, opts, stack.report, opts.Parallelism)
+	default:
+		return stack.modules.RunModules(ctx, opts, stack.report, opts.Parallelism)
+	}
+}
+
+func (stack *RunnerPoolStack) GetModuleRunGraph(terraformCommand string) ([]TerraformModules, error) {
+	var order DependencyOrder
+	if terraformCommand == "destroy" {
+		order = ReverseOrder
+	} else {
+		order = NormalOrder
+	}
+	runningModules, err := stack.modules.ToRunningModules(order, stack.report, stack.terragruntOptions)
+	if err != nil {
+		return nil, err
+	}
+	const maxDepth = 1000
+	groups := runningModules.toTerraformModuleGroups(maxDepth)
+	return groups, nil
+}
+
+func (stack *RunnerPoolStack) ListStackDependentModules() map[string][]string {
+	dependentModules := make(map[string][]string)
+	for _, module := range stack.modules {
+		if len(module.Dependencies) != 0 {
+			for _, dep := range module.Dependencies {
+				dependentModules[dep.Path] = util.RemoveDuplicatesFromList(append(dependentModules[dep.Path], module.Path))
+			}
+		}
+	}
+	for {
+		noUpdates := true
+		for module, dependents := range dependentModules {
+			for _, dependent := range dependents {
+				initialSize := len(dependentModules[module])
+				list := util.RemoveDuplicatesFromList(append(dependentModules[module], dependentModules[dependent]...))
+				list = util.RemoveElementFromList(list, module)
+				dependentModules[module] = list
+				if initialSize != len(dependentModules[module]) {
+					noUpdates = false
+				}
+			}
+		}
+		if noUpdates {
+			break
+		}
+	}
+	return dependentModules
+}
+
+func (stack *RunnerPoolStack) Modules() TerraformModules {
+	return stack.modules
+}
+
+func (stack *RunnerPoolStack) FindModuleByPath(path string) *TerraformModule {
+	for _, module := range stack.modules {
+		if module.Path == path {
+			return module
+		}
+	}
+	return nil
+}
+
+func (stack *RunnerPoolStack) SetTerragruntConfig(config *config.TerragruntConfig) {
+	stack.childConfig = config
+}
+
+func (stack *RunnerPoolStack) GetTerragruntConfig() *config.TerragruntConfig {
+	return stack.childConfig
+}
+
+func (stack *RunnerPoolStack) SetParseOptions(parserOptions []hclparse.Option) {
+	stack.parserOptions = parserOptions
+}
+
+func (stack *RunnerPoolStack) GetParseOptions() []hclparse.Option {
+	return stack.parserOptions
+}
+
+func (stack *RunnerPoolStack) SetModules(modules TerraformModules) {
+	stack.modules = modules
+}
+
+func (stack *RunnerPoolStack) Lock() {
+	stack.outputMu.Lock()
+}
+
+func (stack *RunnerPoolStack) Unlock() {
+	stack.outputMu.Unlock()
 }
