27aeb16b6e052bbeda9aa1c1ae40a88ec07641cd
Denis O <denis.o@linux.com>
2025-06-12 20:09:45 +0300

    feat: Usage of discovery and queue to build stack modules (#4393)
    * Add runner pool stack
    
    * Runner pool stack
    
    * Modules discovery
    
    * Runner pool building
    
    * stack discovery fixes
    
    * stack builder improvements
    
    * Runner pool parsing
    
    * Add runner pool stack
    
    * Runner pool fix
    
    * Improved printing order
    
    * experiments update
    
    * Docs update
    
    * Go mod update
    
    * Build fix
    
    * Discovery cleanup
    
    * Added modules execution
    
    * Added stack building logic
    
    * Runner pool tests execution
    
    * destroy order update
    
    * Added destroy runner-pool experiment
    
    * Fixed lint issues
    
    * Conversion of module to runNow
    
    * PR comments
    
    * Experiments page update
    
    * Discovery revert
    
    * PR comments
    
    * PR comments
    
    * Lint fixes

--- configstack/stack.go
+++ configstack/stack.go
@@ -5,6 +5,8 @@ package configstack
 import (
 	"context"
 
+	"github.com/gruntwork-io/terragrunt/internal/experiment"
+
 	"github.com/gruntwork-io/terragrunt/config/hclparse"
 	"github.com/gruntwork-io/terragrunt/internal/report"
 	"github.com/gruntwork-io/terragrunt/pkg/log"
@@ -42,7 +44,15 @@ type StackBuilder interface {
 // FindStackInSubfolders finds all the Terraform modules in the subfolders of the working directory of the given TerragruntOptions and
 // assemble them into a Stack object that can be applied or destroyed in a single command
 func FindStackInSubfolders(ctx context.Context, l log.Logger, terragruntOptions *options.TerragruntOptions, opts ...Option) (Stack, error) {
-	// here will be used different implementation of stack builder which will generate own stack implementation
+	if terragruntOptions.Experiments.Evaluate(experiment.RunnerPool) {
+		l.Infof("Using RunnerPoolStackBuilder to build stack for %s", terragruntOptions.WorkingDir)
+
+		builder := NewRunnerPoolStackBuilder()
+
+		return builder.BuildStack(ctx, l, terragruntOptions, opts...)
+	}
+
 	builder := &DefaultStackBuilder{}
+
 	return builder.BuildStack(ctx, l, terragruntOptions, opts...)
 }
