3fb511529abe13bbb4d8668a342ce0c60693a45a
AJ <aj@48k.io>
2025-07-01 08:43:33 +0100

    chore: update external dependencies

--- telemetry/meter.go
+++ telemetry/meter.go
@@ -16,7 +16,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
 )
 
 const (
