d40ca2b0b858dbdb9baacf3be864c003db533b39
Karl Carstensen <karl.carstensen@gmail.com>
2025-09-03 12:52:35 -0700

    Bypass partytown (#4783)
    * Bypass partytown
    
    * GTM in header

--- docs-starlight/astro.config.mjs
+++ docs-starlight/astro.config.mjs
@@ -180,7 +180,7 @@ export default defineConfig({
             type: "text/partytown",
           },
           content: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
