58b14fab116b90f4a836cc01ce99a2d1ec47478a
Denis O <denis.o@linux.com>
2025-09-26 09:19:43 +0100

    GCP tests fixes

--- test/integration_gcp_test.go
+++ test/integration_gcp_test.go
@@ -118,7 +118,7 @@ func TestGcpBootstrapBackendWithoutVersioning(t *testing.T) {
 
 	validateGCSBucketExistsAndIsLabeled(t, terraformRemoteStateGcpRegion, gcsBucketName, nil)
 
-	_, stderr, err := helpers.RunTerragruntCommandWithOutput(t, "terragrunt --non-interactive --log-level debug --working-dir "+rootPath+" --feature disable_versioning=true backend delete --all")
+	_, stderr, err := helpers.RunTerragruntCommandWithOutput(t, "terragrunt --non-interactive --log-level debug --working-dir "+rootPath+" backend delete --all --feature disable_versioning=true")
 	require.NoError(t, err)
 	assert.Contains(t, stderr, "Run failed")
 
