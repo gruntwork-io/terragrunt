82eb10f764046a6b3e28077822db150412dd7f43
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-10-07 16:41:59 -0400

    fix: Codespelling

--- cli/flags/shared/shared.go
+++ cli/flags/shared/shared.go
@@ -1,7 +1,7 @@
 // Package shared provides flags that are shared by multiple commands.
 //
 // This package is underutilized right now, as some more serious refactoring is needed to make sure all
-// shared flags use this package instead of re-using flags from other commands.
+// shared flags use this package instead of reusing flags from other commands.
 package shared
 
 import (
