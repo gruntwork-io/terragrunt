757aabff2fa5ef487c37a967c2c94e720984a339
Denis O <denis.o@linux.com>
2025-10-13 15:57:33 +0100

    Cache fixes

--- test/integration_serial_test.go
+++ test/integration_serial_test.go
@@ -828,7 +828,7 @@ func TestRunnerPoolTelemetry(t *testing.T) {
 }
 
 func TestVersionIsInvokedInDifferentDirectory(t *testing.T) {
-	run.ClearVersionCache(t.Context())
+	run.ClearVersionCache()
 
 	tmpEnvPath := helpers.CopyEnvironment(t, testFixtureVersionInvocation)
 	helpers.CleanupTerraformFolder(t, tmpEnvPath)
@@ -851,7 +851,7 @@ func TestVersionIsInvokedInDifferentDirectory(t *testing.T) {
 }
 
 func TestVersionIsInvokedOnlyOnce(t *testing.T) {
-	run.ClearVersionCache(t.Context())
+	run.ClearVersionCache()
 
 	tmpEnvPath := helpers.CopyEnvironment(t, testFixtureDependencyOutput)
 	helpers.CleanupTerraformFolder(t, tmpEnvPath)
