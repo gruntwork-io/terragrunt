b8af0ee1e7d8c4ac252b60ed2f1bdd62d3450df8
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-10-15 22:06:34 -0400

    feat: Adding `filter` experiment

--- internal/experiment/experiment.go
+++ internal/experiment/experiment.go
@@ -32,8 +32,8 @@ const (
 	//
 	// Only works with OpenTofu version >= 1.10.
 	AutoProviderCacheDir = "auto-provider-cache-dir"
-	// FilterFlag is the experiment that enables usage of the filter flag for filtering components
-	FilterFlag = "filter-flag"
+	// Filter is the experiment that enables filter syntax for component selection
+	Filter = "filter"
 )
 
 const (
@@ -77,7 +77,7 @@ func NewExperiments() Experiments {
 			Status: StatusCompleted,
 		},
 		{
-			Name: FilterFlag,
+			Name: Filter,
 		},
 	}
 }
