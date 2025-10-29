b6c6911e5c367ce11e8b4fdafa53148dd5f10b09
Denis O <denis.o@linux.com>
2025-10-29 23:08:09 +0000

    chore: goimports update

--- test/integration_parse_test.go
+++ test/integration_parse_test.go
@@ -34,7 +34,7 @@ var knownBadFiles = []string{
 	"fixtures/hclfmt-errors/invalid-character/terragrunt.hcl",
 	"fixtures/hclfmt-errors/invalid-key/terragrunt.hcl",
 	"fixtures/hclvalidate/second/a/terragrunt.hcl",
-    "fixtures/parsing/exposed-include-with-deprecated-inputs/compcommon.hcl",
+	"fixtures/parsing/exposed-include-with-deprecated-inputs/compcommon.hcl",
 	"fixtures/scaffold/with-shell-and-hooks/.boilerplate/terragrunt.hcl",
 	"fixtures/scaffold/with-shell-commands/.boilerplate/terragrunt.hcl",
 }
