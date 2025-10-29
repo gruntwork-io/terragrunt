d37d3ce05d8f7e357c883e3a2669d6076d94bc64
Denis O <denis.o@linux.com>
2025-10-29 21:58:18 +0000

    add tf file

--- test/fixtures/regressions/parsing-run-all-with-generate/services/test1/main.tf
+++ test/fixtures/regressions/parsing-run-all-with-generate/services/test1/main.tf
@@ -1,19 +1,19 @@
 variable "name" {
-    type = string
+  type = string
 }
 
 output "service_name" {
-    value = var.name
+  value = var.name
 }
 
 output "service_desired_count_initial_value" {
-    value = 1
+  value = 1
 }
 
 output "docker_images" {
-    value = [
-        {
-            repository = "123456789012.dkr.ecr.us-east-1.amazonaws.com/${var.name}-FAKE"
-        }
-    ]
+  value = [
+    {
+      repository = "123456789012.dkr.ecr.us-east-1.amazonaws.com/${var.name}-FAKE"
+    }
+  ]
 }
