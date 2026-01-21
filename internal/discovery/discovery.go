94491f90f79daac956c940af766491bfc5801dbb
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-01-21 14:49:45 -0500

    fix: Cloning readFiles slice in `sanitizeReadFiles`

--- internal/discovery/discovery.go
+++ internal/discovery/discovery.go
@@ -250,7 +250,7 @@ func (d *Discovery) SetParseOptions(options []hclparse.Option) {
 // WithOptions ingests runner options and applies any discovery-relevant settings.
 // Currently, it extracts HCL parser options provided via common.ParseOptionsProvider
 // and forwards them to discovery's parser configuration.
-func (d *Discovery) WithOptions(opts ...interface{}) *Discovery {
+func (d *Discovery) WithOptions(opts ...any) *Discovery {
 	var parserOptions []hclparse.Option
 
 	for _, opt := range opts {
@@ -505,6 +505,8 @@ func sanitizeReadFiles(files []string) []string {
 	if len(files) == 0 {
 		return []string{}
 	}
+
+	files = slices.Clone(files)
 
 	files = slices.DeleteFunc(files, func(file string) bool {
 		return len(file) == 0
