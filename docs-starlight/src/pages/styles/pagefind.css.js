ca237da77eeac07782e15c9568fbbe8cf0d89f39
Karl <karl.carstensen@gmail.com>
2026-02-06 10:59:36 -0800

    Pagefind css

--- docs-starlight/src/pages/styles/pagefind.css.js
+++ docs-starlight/src/pages/styles/pagefind.css.js
@@ -1,7 +1,7 @@
-import globalCss from '../../styles/global.css?raw';
+import pagefindCss from '../../styles/pagefind.css?raw';
 
 export async function GET() {
-  return new Response(globalCss, {
+  return new Response(pagefindCss, {
     headers: {
       'Content-Type': 'text/css',
       'Cache-Control': 'public, max-age=3600',
