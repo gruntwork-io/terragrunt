be743e5f965f82e5f4e6ae374790180ecb73939e
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-04-10 07:49:31 -0400

    docs: Adding TGS docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -192,7 +192,7 @@ export default defineConfig({
 
     // Redirects to external sites.
     "/terragrunt-ambassador": "https://terragrunt.com/terragrunt-ambassador",
-    "/terragrunt-scale": "https://terragrunt.com/terragrunt-scale",
+    "/terragrunt-scale": "/terragrunt-scale/overview/",
     "/contact/": "https://gruntwork.io/contact",
     "/commercial-support/": "https://gruntwork.io/support",
     "/cookie-policy/": "https://gruntwork.io/legal/cookie-policy/",
