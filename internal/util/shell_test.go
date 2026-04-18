b76e434a42ba19bc1af7ccda59d0c86533c426c7
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-04-18 12:42:46 -0400

    chore: Swapping over testing `IsCommandExecutable` to `memCmd`

--- internal/util/shell_test.go
+++ internal/util/shell_test.go
@@ -1,6 +1,8 @@
 package util_test
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/gruntwork-io/terragrunt/internal/util"
@@ -8,14 +10,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestExistingCommand(t *testing.T) {
+func TestIsCommandExecutable(t *testing.T) {
 	t.Parallel()
 
-	assert.True(t, util.IsCommandExecutable(vexec.NewOSExec(), t.Context(), "pwd"))
-}
+	testCases := []struct {
+		name   string
+		result vexec.Result
+		want   bool
+	}{
+		{
+			name:   "handler returns clean exit",
+			result: vexec.Result{},
+			want:   true,
+		},
+		{
+			name:   "handler returns non-zero exit",
+			result: vexec.Result{ExitCode: 1},
+			want:   false,
+		},
+		{
+			name:   "handler returns spawn error",
+			result: vexec.Result{Err: errors.New("fork/exec: no such file")},
+			want:   false,
+		},
+		{
+			name:   "handler writes output but exits clean",
+			result: vexec.Result{Stdout: []byte("hello"), Stderr: []byte("warn")},
+			want:   true,
+		},
+	}
 
-func TestNotExistingCommand(t *testing.T) {
-	t.Parallel()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	assert.False(t, util.IsCommandExecutable(vexec.NewOSExec(), t.Context(), "not-existing-command", "--version"))
+			e := vexec.NewMemExec(func(_ context.Context, _ vexec.Invocation) vexec.Result {
+				return tc.result
+			})
+
+			got := util.IsCommandExecutable(e, t.Context(), "whatever", "-version")
+			assert.Equal(t, tc.want, got)
+		})
+	}
 }
