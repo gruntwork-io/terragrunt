3c8c2cb90649f9bad294349505ab31a0cb87c0be
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-04-18 12:38:17 -0400

    chore: Integrating `vexec` into `IsCommandExecutable`

--- internal/util/shell_test.go
+++ internal/util/shell_test.go
@@ -4,17 +4,18 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terragrunt/internal/util"
+	"github.com/gruntwork-io/terragrunt/internal/vexec"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExistingCommand(t *testing.T) {
 	t.Parallel()
 
-	assert.True(t, util.IsCommandExecutable(t.Context(), "pwd"))
+	assert.True(t, util.IsCommandExecutable(vexec.NewOSExec(), t.Context(), "pwd"))
 }
 
 func TestNotExistingCommand(t *testing.T) {
 	t.Parallel()
 
-	assert.False(t, util.IsCommandExecutable(t.Context(), "not-existing-command", "--version"))
+	assert.False(t, util.IsCommandExecutable(vexec.NewOSExec(), t.Context(), "not-existing-command", "--version"))
 }
