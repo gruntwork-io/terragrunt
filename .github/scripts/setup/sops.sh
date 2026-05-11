880b3a55e48059c8e95c4bc3e6760b1a51882cc3
Denis O <denis.o@linux.com>
2026-05-11 19:21:51 +0000

    chore: CICD fixes

--- .github/scripts/setup/sops.sh
+++ .github/scripts/setup/sops.sh
@@ -3,3 +3,8 @@
 set -euo pipefail
 
 gpg --import --no-tty --batch --yes ./test/fixtures/sops/test_pgp_key.asc
+
+# Export to legacy pubring.gpg/secring.gpg for SOPS v3.13's Go PGP backend.
+gpg --export >"$HOME/.gnupg/pubring.gpg"
+gpg --export-secret-keys >"$HOME/.gnupg/secring.gpg"
+chmod 600 "$HOME/.gnupg/secring.gpg"
