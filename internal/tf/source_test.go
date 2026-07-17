84746f662c593df9cb264b3ee63824514369e93c
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-07-17 13:35:03 -0400

    chore: Use named fields

--- internal/tf/source_test.go
+++ internal/tf/source_test.go
@@ -28,67 +28,98 @@ func TestSplitSourceUrl(t *testing.T) {
 		expectedSo         string
 		expectedModulePath string
 	}{
-		{"root-path-only-no-double-slash", "/foo", "/foo", ""},
-		{"parent-path-one-child-no-double-slash", "/foo/bar", "/foo/bar", ""},
 		{
-			"parent-path-multiple-children-no-double-slash",
-			"/foo/bar/baz/blah",
-			"/foo/bar/baz/blah",
-			"",
-		},
-		{"relative-path-no-children-no-double-slash", "../foo", "../foo", ""},
-		{"relative-path-one-child-no-double-slash", "../foo/bar", "../foo/bar", ""},
-		{
-			"relative-path-multiple-children-no-double-slash",
-			"../foo/bar/baz/blah",
-			"../foo/bar/baz/blah",
-			"",
-		},
-		{"root-path-only-with-double-slash", "/foo//", "/foo", ""},
-		{"parent-path-one-child-with-double-slash", "/foo//bar", "/foo", "bar"},
-		{
-			"parent-path-multiple-children-with-double-slash",
-			"/foo/bar//baz/blah",
-			"/foo/bar",
-			"baz/blah",
-		},
-		{"relative-path-no-children-with-double-slash", "..//foo", "..", "foo"},
-		{"relative-path-one-child-with-double-slash", "../foo//bar", "../foo", "bar"},
-		{
-			"relative-path-multiple-children-with-double-slash",
-			"../foo/bar//baz/blah",
-			"../foo/bar",
-			"baz/blah",
+			name:       "root-path-only-no-double-slash",
+			sourceURL:  "/foo",
+			expectedSo: "/foo",
 		},
 		{
-			"parent-url-one-child-no-double-slash",
-			"ssh://git@github.com/foo/modules.git/foo",
-			"ssh://git@github.com/foo/modules.git/foo",
-			"",
+			name:       "parent-path-one-child-no-double-slash",
+			sourceURL:  "/foo/bar",
+			expectedSo: "/foo/bar",
 		},
 		{
-			"parent-url-multiple-children-no-double-slash",
-			"ssh://git@github.com/foo/modules.git/foo/bar/baz/blah",
-			"ssh://git@github.com/foo/modules.git/foo/bar/baz/blah",
-			"",
+			name:       "parent-path-multiple-children-no-double-slash",
+			sourceURL:  "/foo/bar/baz/blah",
+			expectedSo: "/foo/bar/baz/blah",
 		},
 		{
-			"parent-url-one-child-with-double-slash",
-			"ssh://git@github.com/foo/modules.git//foo",
-			"ssh://git@github.com/foo/modules.git",
-			"foo",
+			name:       "relative-path-no-children-no-double-slash",
+			sourceURL:  "../foo",
+			expectedSo: "../foo",
 		},
 		{
-			"parent-url-multiple-children-with-double-slash",
-			"ssh://git@github.com/foo/modules.git//foo/bar/baz/blah",
-			"ssh://git@github.com/foo/modules.git",
-			"foo/bar/baz/blah",
+			name:       "relative-path-one-child-no-double-slash",
+			sourceURL:  "../foo/bar",
+			expectedSo: "../foo/bar",
 		},
 		{
-			"separate-ref-with-slash",
-			"ssh://git@github.com/foo/modules.git//foo?ref=feature/modules",
-			"ssh://git@github.com/foo/modules.git?ref=feature/modules",
-			"foo",
+			name:       "relative-path-multiple-children-no-double-slash",
+			sourceURL:  "../foo/bar/baz/blah",
+			expectedSo: "../foo/bar/baz/blah",
+		},
+		{
+			name:       "root-path-only-with-double-slash",
+			sourceURL:  "/foo//",
+			expectedSo: "/foo",
+		},
+		{
+			name:               "parent-path-one-child-with-double-slash",
+			sourceURL:          "/foo//bar",
+			expectedSo:         "/foo",
+			expectedModulePath: "bar",
+		},
+		{
+			name:               "parent-path-multiple-children-with-double-slash",
+			sourceURL:          "/foo/bar//baz/blah",
+			expectedSo:         "/foo/bar",
+			expectedModulePath: "baz/blah",
+		},
+		{
+			name:               "relative-path-no-children-with-double-slash",
+			sourceURL:          "..//foo",
+			expectedSo:         "..",
+			expectedModulePath: "foo",
+		},
+		{
+			name:               "relative-path-one-child-with-double-slash",
+			sourceURL:          "../foo//bar",
+			expectedSo:         "../foo",
+			expectedModulePath: "bar",
+		},
+		{
+			name:               "relative-path-multiple-children-with-double-slash",
+			sourceURL:          "../foo/bar//baz/blah",
+			expectedSo:         "../foo/bar",
+			expectedModulePath: "baz/blah",
+		},
+		{
+			name:       "parent-url-one-child-no-double-slash",
+			sourceURL:  "ssh://git@github.com/foo/modules.git/foo",
+			expectedSo: "ssh://git@github.com/foo/modules.git/foo",
+		},
+		{
+			name:       "parent-url-multiple-children-no-double-slash",
+			sourceURL:  "ssh://git@github.com/foo/modules.git/foo/bar/baz/blah",
+			expectedSo: "ssh://git@github.com/foo/modules.git/foo/bar/baz/blah",
+		},
+		{
+			name:               "parent-url-one-child-with-double-slash",
+			sourceURL:          "ssh://git@github.com/foo/modules.git//foo",
+			expectedSo:         "ssh://git@github.com/foo/modules.git",
+			expectedModulePath: "foo",
+		},
+		{
+			name:               "parent-url-multiple-children-with-double-slash",
+			sourceURL:          "ssh://git@github.com/foo/modules.git//foo/bar/baz/blah",
+			expectedSo:         "ssh://git@github.com/foo/modules.git",
+			expectedModulePath: "foo/bar/baz/blah",
+		},
+		{
+			name:               "separate-ref-with-slash",
+			sourceURL:          "ssh://git@github.com/foo/modules.git//foo?ref=feature/modules",
+			expectedSo:         "ssh://git@github.com/foo/modules.git?ref=feature/modules",
+			expectedModulePath: "foo",
 		},
 	}
 
@@ -118,55 +149,55 @@ func TestToSourceUrl(t *testing.T) {
 		expectedSourceURL string
 	}{
 		{
-			"https://github.com/gruntwork-io/repo-name",
-			"git::https://github.com/gruntwork-io/repo-name.git",
+			sourceURL:         "https://github.com/gruntwork-io/repo-name",
+			expectedSourceURL: "git::https://github.com/gruntwork-io/repo-name.git",
 		},
 		{
-			"git::https://github.com/gruntwork-io/repo-name",
-			"git::https://github.com/gruntwork-io/repo-name",
+			sourceURL:         "git::https://github.com/gruntwork-io/repo-name",
+			expectedSourceURL: "git::https://github.com/gruntwork-io/repo-name",
 		},
 		{
-			"https://github.com/gruntwork-io/repo-name//modules/module-name",
-			"git::https://github.com/gruntwork-io/repo-name.git//modules/module-name",
+			sourceURL:         "https://github.com/gruntwork-io/repo-name//modules/module-name",
+			expectedSourceURL: "git::https://github.com/gruntwork-io/repo-name.git//modules/module-name",
 		},
 		{
-			"ssh://github.com/gruntwork-io/repo-name//modules/module-name",
-			"ssh://github.com/gruntwork-io/repo-name//modules/module-name",
+			sourceURL:         "ssh://github.com/gruntwork-io/repo-name//modules/module-name",
+			expectedSourceURL: "ssh://github.com/gruntwork-io/repo-name//modules/module-name",
 		},
 		{
-			"https://gitlab.com/catamphetamine/libphonenumber-js",
-			"git::https://gitlab.com/catamphetamine/libphonenumber-js.git",
+			sourceURL:         "https://gitlab.com/catamphetamine/libphonenumber-js",
+			expectedSourceURL: "git::https://gitlab.com/catamphetamine/libphonenumber-js.git",
 		},
 		{
-			"https://bitbucket.org/atlassian/aws-ecr-push-image",
-			"git::https://bitbucket.org/atlassian/aws-ecr-push-image.git",
+			sourceURL:         "https://bitbucket.org/atlassian/aws-ecr-push-image",
+			expectedSourceURL: "git::https://bitbucket.org/atlassian/aws-ecr-push-image.git",
 		},
 		{
-			"http://bitbucket.org/atlassian/aws-ecr-push-image",
-			"git::https://bitbucket.org/atlassian/aws-ecr-push-image.git",
+			sourceURL:         "http://bitbucket.org/atlassian/aws-ecr-push-image",
+			expectedSourceURL: "git::https://bitbucket.org/atlassian/aws-ecr-push-image.git",
 		},
 		{
-			"https://s3-eu-west-1.amazonaws.com/modules/vpc.zip",
-			"https://s3-eu-west-1.amazonaws.com/modules/vpc.zip",
+			sourceURL:         "https://s3-eu-west-1.amazonaws.com/modules/vpc.zip",
+			expectedSourceURL: "https://s3-eu-west-1.amazonaws.com/modules/vpc.zip",
 		},
 		// Public GCS URLs route through the http(s) getter (no GCP creds), matching
 		// how public S3 URLs are handled. To force the credentialed gcs getter
 		// callers must use the explicit `gcs::` forced-getter prefix.
 		{
-			"https://www.googleapis.com/storage/v1/modules/foomodule.zip",
-			"https://www.googleapis.com/storage/v1/modules/foomodule.zip",
+			sourceURL:         "https://www.googleapis.com/storage/v1/modules/foomodule.zip",
+			expectedSourceURL: "https://www.googleapis.com/storage/v1/modules/foomodule.zip",
 		},
 		{
-			"gcs::https://www.googleapis.com/storage/v1/modules/foomodule.zip",
-			"gcs::https://www.googleapis.com/storage/v1/modules/foomodule.zip",
+			sourceURL:         "gcs::https://www.googleapis.com/storage/v1/modules/foomodule.zip",
+			expectedSourceURL: "gcs::https://www.googleapis.com/storage/v1/modules/foomodule.zip",
 		},
 		{
-			"git::https://name@dev.azure.com/name/project-name/_git/repo-name",
-			"git::https://name@dev.azure.com/name/project-name/_git/repo-name",
+			sourceURL:         "git::https://name@dev.azure.com/name/project-name/_git/repo-name",
+			expectedSourceURL: "git::https://name@dev.azure.com/name/project-name/_git/repo-name",
 		},
 		{
-			"https://repository.rnd.net/artifactory/generic-production-iac/tf-auto-azr-iam.2.6.0.zip",
-			"https://repository.rnd.net/artifactory/generic-production-iac/tf-auto-azr-iam.2.6.0.zip",
+			sourceURL:         "https://repository.rnd.net/artifactory/generic-production-iac/tf-auto-azr-iam.2.6.0.zip",
+			expectedSourceURL: "https://repository.rnd.net/artifactory/generic-production-iac/tf-auto-azr-iam.2.6.0.zip",
 		},
 	}
 
