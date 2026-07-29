9de8dc05f4051c025c1b5ed0964961fe9124ce1e
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-07-29 14:57:43 -0400

    chore: Pin exact provider versions for terralith to terragrunt guide

--- test/fixtures/auto-provider-cache-dir/heavy/unit/main.tf
+++ test/fixtures/auto-provider-cache-dir/heavy/unit/main.tf
@@ -2,55 +2,55 @@ terraform {
   required_providers {
     aws = {
       source  = "registry.opentofu.org/hashicorp/aws"
-      version = "~> 6.0"
+      version = "6.28.0"
     }
     google = {
       source  = "registry.opentofu.org/hashicorp/google"
-      version = "~> 6.0"
+      version = "6.50.0"
     }
     azurerm = {
       source  = "registry.opentofu.org/hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "4.58.0"
     }
     kubernetes = {
       source  = "registry.opentofu.org/hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "2.38.0"
     }
     helm = {
       source  = "registry.opentofu.org/hashicorp/helm"
-      version = "~> 3.0"
+      version = "3.1.1"
     }
     vault = {
       source  = "registry.opentofu.org/hashicorp/vault"
-      version = "~> 5.0"
+      version = "5.6.0"
     }
     consul = {
       source  = "registry.opentofu.org/hashicorp/consul"
-      version = "~> 2.0"
+      version = "2.22.1"
     }
     nomad = {
       source  = "registry.opentofu.org/hashicorp/nomad"
-      version = "~> 2.0"
+      version = "2.5.2"
     }
     datadog = {
       source  = "registry.opentofu.org/DataDog/datadog"
-      version = "~> 3.0"
+      version = "3.85.0"
     }
     github = {
       source  = "registry.opentofu.org/integrations/github"
-      version = "~> 6.0"
+      version = "6.10.2"
     }
     tls = {
       source  = "registry.opentofu.org/hashicorp/tls"
-      version = "~> 4.0"
+      version = "4.1.0"
     }
     random = {
       source  = "registry.opentofu.org/hashicorp/random"
-      version = "~> 3.0"
+      version = "3.8.0"
     }
     null = {
       source  = "registry.opentofu.org/hashicorp/null"
-      version = "~> 3.0"
+      version = "3.2.4"
     }
   }
 }
