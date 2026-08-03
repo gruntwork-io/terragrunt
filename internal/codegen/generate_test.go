23e6cfc182bdf9ccb71cc4bbea26ba0513347b6b
somaz <genius5711@gmail.com>
2026-08-03 11:28:17 +0900

    test: Cover both remote_state.generate forms for the overwrite_terragrunt_or_skip gate

--- internal/codegen/generate_test.go
+++ internal/codegen/generate_test.go
@@ -914,7 +914,7 @@ func TestWriteToFileOverwriteTerragruntOrSkip(t *testing.T) {
 			}
 
 			l := logger.CreateLogger()
-			require.NoError(t, codegen.WriteToFile(t.Context(), l, venv.OSVenv(), "", &config))
+			require.NoError(t, codegen.WriteToFile(t.Context(), l, venvtest.NewOSWithEmptyEnv(), "", &config))
 
 			fileContent, err := os.ReadFile(path)
 			require.NoError(t, err)
@@ -955,7 +955,7 @@ func TestWriteToFileOverwriteTerragruntStillErrorsOnHandWrittenFile(t *testing.T
 
 	var existsErr codegen.GenerateFileExistsError
 
-	require.ErrorAs(t, codegen.WriteToFile(t.Context(), l, venv.OSVenv(), "", &config), &existsErr)
+	require.ErrorAs(t, codegen.WriteToFile(t.Context(), l, venvtest.NewOSWithEmptyEnv(), "", &config), &existsErr)
 
 	fileContent, err := os.ReadFile(path)
 	require.NoError(t, err)
