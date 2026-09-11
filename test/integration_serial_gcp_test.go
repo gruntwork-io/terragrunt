bf1fd39cfc3c1e6a21a3e7594c9adffba6b9fd6a
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-09-11 07:04:03 -0400

    chore: Fixing cloud credentials test env semantics (#6826)

--- test/integration_serial_gcp_test.go
+++ test/integration_serial_gcp_test.go
@@ -16,13 +16,14 @@ import (
 )
 
 func TestGcpCorrectlyMirrorsTerraformGCPAuth(t *testing.T) {
-	// We need to ensure Terragrunt works correctly when GOOGLE_CREDENTIALS are specified.
-	// There is no true way to properly unset env vars from the environment, but we still try
-	// to unset the CI credentials during this test.
+	// Terragrunt has to work when only GOOGLE_CREDENTIALS is set, so the CI credential
+	// is taken out of the environment for this test. t.Setenv registers the restore,
+	// and the Unsetenv that follows takes the variable out rather than blanking it.
 	defaultCreds := os.Getenv("GCLOUD_SERVICE_KEY")
-	defer os.Setenv("GCLOUD_SERVICE_KEY", defaultCreds) //nolint:usetesting // restores the CI credential this test unsets
 
+	t.Setenv("GCLOUD_SERVICE_KEY", "")
 	os.Unsetenv("GCLOUD_SERVICE_KEY")
+
 	t.Setenv("GOOGLE_CREDENTIALS", defaultCreds)
 
 	tmpEnvPath := helpers.CopyEnvironment(t, testFixtureGcsPath)
